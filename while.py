19e23395dcfafebb6c49546b3d58bb3335b940e8
AlgoClassWork <kyxec.play@gmail.com>
2024-06-10 11:46:57 +0600

    Update while.py

--- while.py
+++ while.py
@@ -5,3 +5,13 @@ while tovar != 0:
     tovar = int(input('Стоимость товара (0 — покупок больше нет):')) 
     total = total + tovar
 print('Стоимость всех покупок:', total)
+
+
+#Подсчёт категорий товаров
+category = input('Категория (end - завершить):')
+count = 0
+while category != 'end':
+    count += 1
+    category = input('Категория (end - завершить):')
+
+print('Всего категорий товаров:', count)
