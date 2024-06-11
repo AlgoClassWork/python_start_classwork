68410165ec0e4ddf548d2551149d025244f81c09
AlgoClassWork <kyxec.play@gmail.com>
2024-06-11 11:00:27 +0600

    Update for.py

--- for.py
+++ for.py
@@ -3,3 +3,12 @@ for i in range(3):
     wish = input('Введите предпочтение:')
     print('Предпочтение учтено')
 print('Система рекомендаций настроена!')
+
+
+
+total = 0
+count = int(input('Количество оценок:'))
+for i in range(count):
+    mark = int(input('Оценка:'))
+    total += mark
+print('Среднее:', total/count)
