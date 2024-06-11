f35896a4ca4bb752f02ca61be541b654052e99d6
AlgoClassWork <kyxec.play@gmail.com>
2024-06-11 17:17:30 +0600

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,16 @@
+#Данные одного из читателей. Не менять!
+student_card = {'номер': '324', 'фамилия': 'Иванов'}
+print('Добро пожаловать!', student_card)
+otvet = input('Личный кабинет: 1 - взять, 2 - вернуть, 3 - домой')
+while otvet != '3':
+    if otvet == '1':
+        name = input('Введите название:')
+        student_card['долг'] = name
+        print('Карточка читателя:',student_card)
+    if otvet == '2':
+        if 'долг' in student_card:
+            del student_card['долг']
+        print('Карточка читателя:',student_card)
+    otvet = input('Личный кабинет: 1 - взять, 2 - вернуть, 3 - домой')
+
+print('Ждём вас:',student_card)
