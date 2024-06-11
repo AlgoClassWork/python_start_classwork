a269e96257a7caec42822816f26580b3acccca7c
AlgoClassWork <kyxec.play@gmail.com>
2024-06-11 17:48:21 +0600

    Update dict.py

--- dict.py
+++ dict.py
@@ -14,3 +14,20 @@ while otvet != '3':
     otvet = input('Личный кабинет: 1 - взять, 2 - вернуть, 3 - домой')
 
 print('Ждём вас:',student_card)
+
+
+
+my_shelf = dict()
+author = input('Введите автора (q - завершить):')
+while author != 'q':
+   books = list()
+   book = input('Введите книгу (s - стоп):')
+   while book != 's':
+       books.append(book)
+       book = input('Введите книгу (s - стоп):')
+   my_shelf[author] = books
+   author = input('Введите автора (q - завершить):')
+
+for author in my_shelf:
+   print(author, '-', my_shelf[author])
+
