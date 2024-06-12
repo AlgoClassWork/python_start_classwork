592eff90f9725501b75b829eb014cafb7d49ee94
AlgoClassWork <kyxec.play@gmail.com>
2024-06-12 15:12:15 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from random import randint
+
 print('Здравствуйте меня зовут бот Генадий')
 print('Вот что я умею: шутить рекомендовать оняме')
 print('Могу с тобой сыграть и продать тебе хлам')
@@ -15,6 +17,19 @@ while otvet.find('пока') == -1:
             print('Атака титаника')
         else:
             print('Таких жанров я не знаю')
+    elif otvet.find('игр') != -1:
+        win_num = randint(1,100)
+        for i in range(7):
+            num = int(input('Введите число:'))
+            if num > win_num:
+                print('Загаданое число меньше')
+            elif num < win_num:
+                print('Загаданое число больше')
+            else:
+                print('Вы выйграли пачку сухариков!')
+                break
+        if win_num != num:
+            print('Азазазаза ты ...')
     else:
         print('Я тебя не понимаю!')
     otvet = input('Что бы вы от меня хотели?').lower()
