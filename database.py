7ccb698fa5b0a2c169d36a5f1c0770fb0fb5a572
AlgoClassWork <kyxec.play@gmail.com>
2024-06-12 17:37:52 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 database = {
     'Гейб Ньюэл': {
         'должность': 'директор',
-        'эффективность': 99,
+        'эффективность': 10,
         'проекты': ['dota 2','cs-go 2','half-life 2']
     },
     'Илон Маск': {
         'должность': 'гений',
-        'эффективность': 50,
+        'эффективность': 30,
         'проекты': ['tesla','space-x','pay-pal']
     },
     'Павел Вольнадуров': {
@@ -15,3 +15,25 @@ database = {
         'проекты': ['telegram','comedy-club','V-Odnoklassnikah']
     },
 }
+
+question = input('Что бы хотели узнать о наших сотрудниках?')
+while question != 'off':
+    if question == 'фамилии':
+        print('Фамилии сотрудников:')
+        for surname in database:
+            print('-',surname.split()[1])
+    elif question == 'эффективность':
+        effectivnost = []
+        for surname in database:
+            effectivnost.append(database[surname]['эффективность'])
+        effectivnost.sort()
+        max_effect = effectivnost[-1]
+        samyi_krutoi = ''
+        for surname in database:
+            if database[surname]['эффективность'] == max_effect:
+                samyi_krutoi = surname
+        print('Самый эффективный сотрудник:', samyi_krutoi)
+            
+
+    question = input('Что бы хотели узнать о наших сотрудниках?')
+    
