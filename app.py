b3cac262cb089b1bc8b3e16707f739ccf1cf35a0
AlgoClassWork <kyxec.play@gmail.com>
2024-06-19 16:54:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
         QApplication, QWidget, QVBoxLayout, 
         QGroupBox, QRadioButton,  
         QPushButton, QLabel)
+
+#БАЗА ДАННЫХ
+class Question():
+    def __init__(self,question,right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+question_list = []
+question_list.append(Question('Когда родился Австрийский художник','568','1350','888','1488'))
+question_list.append(Question('Почему небо голубое','из за Голубей','низнаю','хз','потому что'))
+question_list.append(Question('Почему в рф запретили голубой цвет','потому что его нет на флаге','из за голубей','захотелось','низнаю'))
+       
+
 
 #ИНТЕРФЕЙС
 app = QApplication([])
@@ -27,19 +42,43 @@ layout_ans2.addWidget(rbtn_3) # два ответа во второй столб
 layout_ans2.addWidget(rbtn_4)
 
 RadioGroupBox.setLayout(layout_ans2) # готова "панель" с вариантами ответов 
-
 #ФОРМА С РЕЗУЛЬТАТОМ
+AnsGroupBox = QGroupBox("Результаты теста")
+lb_Result = QLabel('прав ты или нет?')
+lb_Correct = QLabel('ответ будет тут!')
 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result)
+layout_res.addWidget(lb_Correct)
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()
 #РАЗМЕЩЕНИЕ ВИДЖЕТОВ
 layout_card = QVBoxLayout()
 
 layout_card.addWidget(lb_Question)
 layout_card.addWidget(RadioGroupBox)
-
+layout_card.addWidget(AnsGroupBox)
 layout_card.addWidget(btn_OK)
 #ФУНКЦИОНАЛ
 
+def check_answer():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+def next_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+
+def click_OK():
+    if btn_OK.text() == 'Ответить':
+        check_answer() # проверка ответа
+    else:
+        next_question() # следующий вопрос
+
 #ПОДПИСКИ
+btn_OK.clicked.connect(click_OK)
 
 #ЗАПУСК
 window.setLayout(layout_card)
