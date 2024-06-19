0aedf00c113a8e50d7b90c011b899176e07bb7d4
AlgoClassWork <kyxec.play@gmail.com>
2024-06-19 10:15:28 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,32 +1,26 @@
 from turtle import *
 
-class Sprite(Turtle):
-    def __init__(self, x, y, shape, color):
-        Turtle.__init__(self)
-        self.penup()
-        self.goto(x, y)
-        self.color(color)
-        self.shape(shape)
+pensize(2)
+color("red")
+begin_fill()
+circle(50)
+end_fill()
+#допиши программу
+penup()
+goto(-32,37)
+pendown()
 
-    def move_up(self):
-        self.goto(self.xcor(), self.ycor() + 5)
-    def move_down(self):
-        self.goto(self.xcor(), self.ycor() - 5)
-    def move_left(self):
-        self.goto(self.xcor() - 5, self.ycor())
-    def move_right(self):
-        self.goto(self.xcor() + 5, self.ycor())
+color('white')
+begin_fill()
+forward(65)
+left(90)
+forward(20)
+left(90)
+forward(65)
+left(90)
+forward(20)
+left(90)
+end_fill()
 
-
-player = Sprite(0,-150,'circle','black')
-enemy1 = Sprite(-150,-50,'square','red')
-enemy2 = Sprite(150,50,'square','red')
-goal = Sprite(0,159,'triangle','green')
-
-
-scr = player.getscreen()
-scr.listen()
-scr.onkey(player.move_up, 'w')
-scr.onkey(player.move_left, 'a')
-scr.onkey(player.move_right, 'd')
-scr.onkey(player.move_down, 's')
+hideturtle()
+exitonclick()
