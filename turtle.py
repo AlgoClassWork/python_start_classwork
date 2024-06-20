6bd7aafc4ec9fd5c0123106cc9129ab5a270d55f
AlgoClassWork <kyxec.play@gmail.com>
2024-06-20 15:25:25 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,39 +1,41 @@
 from turtle import *
 from random import randint
 
-gitler = Turtle()
-picachu = Turtle()
+def start_race(name,col,y):
+   name.penup()
+   name.goto(-200,y)
+   name.shape('turtle')
+   name.color('black',col)
 
-def start_race(turt,col,y):
-   turt.color('black',col)
-   turt.penup()
-   turt.goto(-200,y)
-   turt.shape('turtle')
+biba = Turtle()
+boba = Turtle()
 
-start_race(turt=gitler,col='red',y=40)
-start_race(turt=picachu,col='yellow',y=-40)
+start_race(name=biba,col='red',y=40)
+start_race(name=boba,col='blue',y=-40)
 
 finish = 200
-stavka = input('На кого вы хотите поставить?').lower()
-while gitler.xcor() < finish and picachu.xcor() < finish:
-   gitler.forward(randint(1,10))
-   picachu.forward(randint(1,10))
 
-winner = max(gitler.xcor(),picachu.xcor())
-if winner == gitler.xcor():
-   gitler.goto(-200,-20)
-   if stavka == 'гитлер':
-      gitler.write('Ты выйграл пачку сухариков',font=('Arial',22,'bold'))
+stavka = input('Кто победит?').lower()
+
+while biba.xcor() < finish and boba.xcor() < finish:
+   biba.forward( randint(1,10) )
+   boba.forward( randint(1,10) )
+
+winner = max(biba.xcor(), boba.xcor())
+
+if biba.xcor() == winner:
+   biba.goto(-200,-20)
+   if stavka == 'биба':
+      biba.write('Вы выйграли пачку сухариков',font=('Arial',20,'bold'))
    else:
-      gitler.write('Твой дом заберут коллекторы',font=('Arial',22,'bold'))
-else:
-   picachu.goto(-200,-20)
-   if stavka == 'пикачу':
-      picachu.write('Ты выйграл пачку сухариков',font=('Arial',22,'bold'))
+      biba.write('Вы проиграли :(',font=('Arial',20,'bold'))
+
+if boba.xcor() == winner:
+   boba.goto(-200,-20)
+   if stavka == 'боба':
+      boba.write('Вы выйграли пачку сухариков',font=('Arial',20,'bold'))
    else:
-      picachu.write('Твой дом заберут коллекторы',font=('Arial',22,'bold'))
+      boba.write('Вы проиграли :(',font=('Arial',20,'bold'))
 
-gitler.hideturtle()
-picachu.hideturtle()
-
-
+biba.hideturtle()
+boba.hideturtle()
