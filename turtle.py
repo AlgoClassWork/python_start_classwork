f933fef13d17eaada76a01251a374205eb0f2eae
AlgoClassWork <kyxec.play@gmail.com>
2024-06-20 11:32:04 +0600

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,34 +1,39 @@
 from turtle import *
+from random import randint
 
-def svetofor(svet):
-    goto(0,90)
-    color('red')
-    if svet.find('красный') != -1:
-        begin_fill()
-        circle(30)
-        end_fill()
-    else:
-        circle(30)
+gitler = Turtle()
+picachu = Turtle()
 
-    goto(0,0)
-    color('yellow')
-    if svet.find('желтый') != -1:
-        begin_fill()
-        circle(30)
-        end_fill()
-    else:
-        circle(30)
+def start_race(turt,col,y):
+   turt.color('black',col)
+   turt.penup()
+   turt.goto(-200,y)
+   turt.shape('turtle')
 
-    goto(0,-90)
-    color('green')
-    if svet.find('зеленый') != -1:
-        begin_fill()
-        circle(30)
-        end_fill()
-    else:
-        circle(30)
+start_race(turt=gitler,col='red',y=40)
+start_race(turt=picachu,col='yellow',y=-40)
 
-otvet = input('Какой сейчас горит свет')
-svetofor(otvet)
+finish = 200
+stavka = input('На кого вы хотите поставить?').lower()
+while gitler.xcor() < finish and picachu.xcor() < finish:
+   gitler.forward(randint(1,10))
+   picachu.forward(randint(1,10))
 
-exitonclick()
+winner = max(gitler.xcor(),picachu.xcor())
+if winner == gitler.xcor():
+   gitler.goto(-200,-20)
+   if stavka == 'гитлер':
+      gitler.write('Ты выйграл пачку сухариков',font=('Arial',22,'bold'))
+   else:
+      gitler.write('Твой дом заберут коллекторы',font=('Arial',22,'bold'))
+else:
+   picachu.goto(-200,-20)
+   if stavka == 'пикачу':
+      picachu.write('Ты выйграл пачку сухариков',font=('Arial',22,'bold'))
+   else:
+      picachu.write('Твой дом заберут коллекторы',font=('Arial',22,'bold'))
+
+gitler.hideturtle()
+picachu.hideturtle()
+
+
