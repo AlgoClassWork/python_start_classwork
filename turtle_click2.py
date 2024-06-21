baad3e77cc95465241ddab1e27e6a3c7e1945cf9
AlgoClassWork <kyxec.play@gmail.com>
2024-06-21 11:48:11 +0600

    Update turtle_click2.py

--- turtle_click2.py
+++ turtle_click2.py
@@ -2,6 +2,15 @@ from turtle import *
 from random import randint
 from time import time
 
+
+def finish(name1,name2,name3):
+  name1_outside = abs( name1.xcor() ) > 200 or abs( name1.ycor() ) > 200 
+  name2_outside = abs( name2.xcor() ) > 200 or abs( name2.ycor() ) > 200 
+  name3_outside = abs( name3.xcor() ) > 200 or abs( name3.ycor() ) > 200 
+  game_over = name1_outside or name2_outside or name3_outside
+  return game_over
+
+
 def start(name,col,angle):
   name.penup()
   name.shape('turtle')
@@ -29,11 +38,11 @@ start(adolf,'black',0)
 start(oleg,'blue',120)
 start(saitama,'red',240)
 
-adolf.onclick(catch_adolf) #new
-oleg.onclick(catch_oleg) #new
-saitama.onclick(catch_saitama) #new
+adolf.onclick(catch_adolf) 
+oleg.onclick(catch_oleg) 
+saitama.onclick(catch_saitama) 
 
-while True:
+while finish(adolf,oleg,saitama) != True:
   move(adolf)
   move(oleg)
   move(saitama)
