fae69514ee2865b88634b33c394a8da8869ef413
AlgoClassWork <kyxec.play@gmail.com>
2024-06-21 15:48:10 +0600

    Update turtle_click2.py

--- turtle_click2.py
+++ turtle_click2.py
@@ -24,9 +24,9 @@ naruto.speed(10)
 naruto.left(240)
 
 def finish(t1,t2,t3):
-  t1_outside = abs(t1.xcor()) > 200 or abs(t1.ycor) > 200 
-  t2_outside = abs(t2.xcor()) > 200 or abs(t2.ycor) > 200 
-  t3_outside = abs(t3.xcor()) > 200 or abs(t3.ycor) > 200 
+  t1_outside = abs(t1.xcor()) > 200 or abs(t1.ycor()) > 200 
+  t2_outside = abs(t2.xcor()) > 200 or abs(t2.ycor()) > 200 
+  t3_outside = abs(t3.xcor()) > 200 or abs(t3.ycor()) > 200 
   game_over = t1_outside or t2_outside or t3_outside 
   return game_over
 
@@ -43,6 +43,8 @@ gleb.onclick(catch_gleb)
 adolf.onclick(catch_adolf)
 naruto.onclick(catch_naruto)
 
+start_time = time() 
+
 while True:
   gleb.forward(5)
   gleb.left( randint(-10,10) )
@@ -50,8 +52,13 @@ while True:
   adolf.left( randint(-10,10) )
   naruto.forward(5)
   naruto.left( randint(-10,10) )
+  if finish(gleb,adolf,naruto):
+    break
+
+end_time = time()
+
 
 gleb.goto(-150,-20)
 gleb.clear()
-gleb.write('Красава!',font=('Arial',20,'bold'))
+gleb.write('Вы продержались! ' + str(int(end_time- start_time)) + 'с'  ,font=('Arial',20,'bold'))
 gleb.hideturtle()
