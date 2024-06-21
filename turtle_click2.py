fc47c27284ff5855f6852039ce0fd5f897e28bff
AlgoClassWork <kyxec.play@gmail.com>
2024-06-21 15:08:57 +0600

    Update turtle_click2.py

--- turtle_click2.py
+++ turtle_click2.py
@@ -2,47 +2,41 @@ from turtle import *
 from random import randint
 from time import time
 
-
-def finish(name1,name2,name3):
-  name1_outside = abs( name1.xcor() ) > 200 or abs( name1.ycor() ) > 200 
-  name2_outside = abs( name2.xcor() ) > 200 or abs( name2.ycor() ) > 200 
-  name3_outside = abs( name3.xcor() ) > 200 or abs( name3.ycor() ) > 200 
-  game_over = name1_outside or name2_outside or name3_outside
-  return game_over
-
-
-def start(name,col,angle):
-  name.penup()
-  name.shape('turtle')
-  name.color('black',col)
-  name.left(angle)
-
-def move(name):
-  name.forward(5)
-  name.left(randint(-10,10))
-
-def catch_adolf(x,y): 
-  adolf.left(180)     
-
-def catch_oleg(x,y):  
-  oleg.left(180)      
-
-def catch_saitama(x,y): 
-  saitama.left(180)     
+gleb = Turtle()
+gleb.penup()
+gleb.shape('turtle')
+gleb.color('black','blue')
+gleb.speed(10)
+gleb.left(0)
 
 adolf = Turtle()
-oleg = Turtle()
-saitama = Turtle()
+adolf.penup()
+adolf.shape('turtle')
+adolf.color('black','white')
+adolf.speed(10)
+adolf.left(120)
 
-start(adolf,'black',0)
-start(oleg,'blue',120)
-start(saitama,'red',240)
+naruto = Turtle()
+naruto.penup()
+naruto.shape('turtle')
+naruto.color('black','orange')
+naruto.speed(10)
+naruto.left(240)
 
-adolf.onclick(catch_adolf) 
-oleg.onclick(catch_oleg) 
-saitama.onclick(catch_saitama) 
+def catch(x,y):
+    gleb.write('Вай!')
+    
+gleb.onclick(catch)
 
-while finish(adolf,oleg,saitama) != True:
-  move(adolf)
-  move(oleg)
-  move(saitama)
+while True:
+  gleb.forward(5)
+  gleb.left( randint(-10,10) )
+  adolf.forward(5)
+  adolf.left( randint(-10,10) )
+  naruto.forward(5)
+  naruto.left( randint(-10,10) )
+
+gleb.goto(-150,-20)
+gleb.clear()
+gleb.write('Красава!',font=('Arial',20,'bold'))
+gleb.hideturtle()
