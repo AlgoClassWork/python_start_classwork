a862bbbe0a95a54911e970f4c25b9e1cc6239b40
AlgoClassWork <kyxec.play@gmail.com>
2024-06-21 11:24:00 +0600

    Update turtle_click2.py

--- turtle_click2.py
+++ turtle_click2.py
@@ -2,16 +2,25 @@ from turtle import *
 from random import randint
 from time import time
 
-adolf = Turtle()
-oleg = Turtle()
-saitama = Turtle()
-
 def start(name,col,angle):
   name.penup()
   name.shape('turtle')
   name.color('black',col)
   name.left(angle)
 
+def move(name):
+  name.forward(5)
+  name.left(randint(-10,10))
+
+adolf = Turtle()
+oleg = Turtle()
+saitama = Turtle()
+
 start(adolf,'black',0)
 start(oleg,'blue',120)
 start(saitama,'red',240)
+
+while True:
+  move(adolf)
+  move(oleg)
+  move(saitama)
