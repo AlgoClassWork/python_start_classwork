097297d8b7014be8b3cfbd689d3175b73a90478e
AlgoClassWork <kyxec.play@gmail.com>
2024-06-24 14:40:37 +0600

    Update simple_paint.py

--- simple_paint.py
+++ simple_paint.py
@@ -4,8 +4,16 @@ picaso = Turtle()
 picaso.width(5)
 picaso.speed(10)
 picaso.shape('circle')
+# Cоздание и настройка обьекта экрана
+screen = picaso.getscreen()
 # Функционал редактора
 def draw(x,y):
     picaso.goto(x,y)
+    
+def move(x,y):
+    picaso.penup()
+    picaso.goto(x,y)
+    picaso.pendown()
 # Подписки на события
 picaso.ondrag(draw)
+screen.onscreenclick(move)
