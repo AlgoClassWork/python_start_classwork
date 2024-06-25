1e36affa61e71a7f98df858ebc83316fc2a1714e
AlgoClassWork <kyxec.play@gmail.com>
2024-06-25 11:39:56 +0600

    Update classy.py

--- classy.py
+++ classy.py
@@ -13,7 +13,7 @@ class Hero:
         print('В руках он держит',self.weapon)
 
     def fight(self,opponent):
-        print('Началось сражение между', self.name, 'и', opponent.name)
+        print('Началось сражение между', self.name, 'и', opponent.name,'\n')
         while self.health > 0 and opponent.health > 0:
             print(self.name,'наносит удар',opponent.name,)
 
@@ -46,11 +46,36 @@ class Hero:
             sleep(1)
             print('\n')
 
-        if self.health > opponent.health:
-            print('Победил', self.name)
-        else:
-            print('Победил',opponent.name)
+knight = Hero('Артур','меч',100,100)
+enemy = Hero('Адольф','сирюкен',90,25)
+dragon = Hero('Смауг','фаербол',250,50)
 
-knight = Hero('Артур','меч',100,10)
-enemy = Hero('Адольф','сирюкен',90,9)
-knight.fight(enemy)
+print('Бравый рыцарь по имени',knight.name,'отправился в логово дракона','\n')
+sleep(2)
+print('На пути нашего рыцарь появилась преграда нацисткий танк','\n')
+sleep(2)
+print('Вы даже не представляете кто в нем сидит','\n')
+sleep(2)
+otvet = input('Хотите узнать и сразится?')
+if otvet == 'да':
+    print('Так точно этот тот самый','\n', enemy.name)
+    sleep(2)
+    knight.fight(enemy)
+    if knight.health > enemy.health:
+        knight.health = 200
+        knight.power *= 2
+        print('Вы выйграли и наконецто добрались до логова дракона','\n')
+        sleep(2)
+        print('можете немного передохнуть секунд так 5','\n')
+        sleep(5)
+        print('ииииии в бой','\n')
+        knight.fight(dragon)
+        if knight.health > dragon.health:
+            print('Поздравляем с победой в сундуке лежит пачка сухариков с сыром')
+        else:
+            print('За это стоило умереть')
+    else:
+        print('Победил',opponent.name,'\n')
+        print('Твое путешествие окончено :(')
+    
+
