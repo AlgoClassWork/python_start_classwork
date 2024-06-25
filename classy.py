1db5c9c070e0c16d1f9a87e86cc6efd91aafcee1
AlgoClassWork <kyxec.play@gmail.com>
2024-06-25 11:25:05 +0600

    Update classy.py

--- classy.py
+++ classy.py
@@ -1,3 +1,6 @@
+from random import randint
+from time import sleep
+
 class Hero:
     def __init__(self,name,weapon,health,power):
         self.name = name
@@ -12,10 +15,37 @@ class Hero:
     def fight(self,opponent):
         print('Началось сражение между', self.name, 'и', opponent.name)
         while self.health > 0 and opponent.health > 0:
-            print(self.name,'наносит удар',opponent.name)
-            opponent.health -= self.power
-            print(opponent.name,'отвечает',self.name)
-            self.health -= opponent.power
+            print(self.name,'наносит удар',opponent.name,)
+
+            udar = randint(1,3)
+            if udar == 1:
+                opponent.health -= 0
+                print('Промах')
+            elif udar == 2:
+                opponent.health -= self.power
+                print('Есть пробитие')
+            else:
+                opponent.health -= self.power * 2
+                print('Сработал крит')
+
+            sleep(1)
+            print('\n')
+
+            print(opponent.name,'отвечает',self.name,)
+            udar = randint(1,3)
+            if udar == 1:
+                self.health -= 0
+                print('Промах')
+            elif udar == 2:
+                self.health -= opponent.power
+                print('Есть пробитие')
+            else:
+                self.health -= opponent.power * 2
+                print('Сработал крит')
+
+            sleep(1)
+            print('\n')
+
         if self.health > opponent.health:
             print('Победил', self.name)
         else:
