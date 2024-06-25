b0c58f9af719899f2cf594d15925ede833db6527
AlgoClassWork <kyxec.play@gmail.com>
2024-06-25 15:21:51 +0600

    Update knight_vs_dragon.py

--- knight_vs_dragon.py
+++ knight_vs_dragon.py
@@ -5,6 +5,19 @@ class Person:
         self.damage = damage
         self.weapon = weapon
 
+    def info(self):
+        print('На поле боя появился',self.name)
+        print('Его секретное оружие это',self.weapon)
+
+    def udar(self,enemy):
+        print(self.name,'наносит удар при помощи',self.weapon,enemy.name)
+        enemy.health -= self.damage
+        print('Здоровье',enemy.health)
+
 knight = Person(name='Олег',health=100,damage=10,weapon='меч')
 razboinik = Person(name='Адольф',health=90,damage=9,weapon='сирюкен')
 dragon = Person(name='Смауг',health=200,damage=50,weapon='фаербол')
+
+while knight.health > 0 and razboinik.health > 0:
+    knight.udar(razboinik)
+    razboinik.udar(knight)
