0b1c40ef7edd522c70033ef9f1bb0e3a3e5d1c72
AlgoClassWork <kyxec.play@gmail.com>
2024-06-25 11:15:12 +0600

    Update classy.py

--- classy.py
+++ classy.py
@@ -9,5 +9,18 @@ class Hero:
         print('На поле битвы появляется',self.name)
         print('В руках он держит',self.weapon)
 
+    def fight(self,opponent):
+        print('Началось сражение между', self.name, 'и', opponent.name)
+        while self.health > 0 and opponent.health > 0:
+            print(self.name,'наносит удар',opponent.name)
+            opponent.health -= self.power
+            print(opponent.name,'отвечает',self.name)
+            self.health -= opponent.power
+        if self.health > opponent.health:
+            print('Победил', self.name)
+        else:
+            print('Победил',opponent.name)
+
 knight = Hero('Артур','меч',100,10)
 enemy = Hero('Адольф','сирюкен',90,9)
+knight.fight(enemy)
