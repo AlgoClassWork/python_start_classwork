643939407582978e0dbd5c8f861d5b534711a80c
AlgoClassWork <kyxec.play@gmail.com>
2024-06-25 10:59:11 +0600

    Create classy.py

---
+++ classy.py
@@ -0,0 +1,12 @@
+class Hero:
+    def __init__(self,name,weapon,health,power):
+        self.name = name
+        self.weapon = weapon
+        self.health = health
+        self.power = power
+    
+    def info(self):
+        print('На поле битвы появляется',self.name)
+
+knight = Hero('Артур','меч',100,10)
+enemy = Hero('Адольф','сирюкен',90,9)
