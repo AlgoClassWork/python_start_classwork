9e2043986641c6a2a1f34f7839663e256cef7130
AlgoClassWork <kyxec.play@gmail.com>
2024-06-26 15:23:26 +0600

    Update hit_it.py

--- hit_it.py
+++ hit_it.py
@@ -8,6 +8,22 @@ class Sprite(Turtle):
         self.shape(forma)
         self.goto(x,y)
         self.setheading(90)
+
+    def move_up(self):
+        self.setheading(90)
+        self.forward(5)
+
+    def move_down(self):
+        self.setheading(270)
+        self.forward(5)
+
+    def move_left(self):
+        self.setheading(180)
+        self.forward(5)
+
+    def move_right(self):
+        self.setheading(0)
+        self.forward(5)
 # Создаем наших игровых персонажей
 player = Sprite(x=0,y=-150,forma='turtle',col='yellow')
 enemy1 = Sprite(x=150,y=50,forma='square',col='red')
