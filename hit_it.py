37eef6fa208d9c7a21128dc3276e88434c7612ea
AlgoClassWork <kyxec.play@gmail.com>
2024-06-26 15:00:14 +0600

    Update hit_it.py

--- hit_it.py
+++ hit_it.py
@@ -1,15 +1,19 @@
 from turtle import *
 
 class Sprite(Turtle):
-    def __init__(self,x,y,col,forma):
+    def __init__(self,col,forma,x,y):
         super().__init__()
         self.penup()
-        self.goto(x,y)
         self.color('black',col)
         self.shape(forma)
+        self.goto(x,y)
         self.setheading(90)
 
-player = Sprite(x=0,y=-150,col='blue',forma='turtle')
-enemy1 = Sprite(x=150,y=50,col='red',forma='square')
-enemy2 = Sprite(x=-150,y=-50,col='red',forma='square')
-goal = Sprite(x=0,y=150,col='green',forma='triangle')
+
+player = Sprite(x=0,y=-150,forma='turtle',col='yellow')
+enemy1 = Sprite(x=150,y=50,forma='square',col='red')
+enemy2 = Sprite(x=-150,y=-50,forma='square',col='red')
+goal = Sprite(x=0,y=150,forma='triangle',col='green')
+
+
+
