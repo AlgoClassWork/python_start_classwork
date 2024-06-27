962c9c33cd826d17f49991b4c6c517fbb3cc55ee
AlgoClassWork <kyxec.play@gmail.com>
2024-06-27 14:58:58 +0600

    Update hit_it.py

--- hit_it.py
+++ hit_it.py
@@ -25,6 +25,13 @@ class Sprite(Turtle):
         self.setheading(0)
         self.forward(5)
 
+    def collide(self,turtle):
+        dist = self.distance(turtle.xcor(),turtle.ycor())
+        if dist < 30:
+            return True
+        else:
+            return False
+            
 
 # Создаем наших игровых персонажей
 player = Sprite(x=0,y=-150,forma='turtle',col='yellow')
@@ -39,6 +46,25 @@ screen.onkey(player.move_up,'Up')
 screen.onkey(player.move_down,'Down')
 screen.onkey(player.move_left,'Left')
 screen.onkey(player.move_right,'Right')
+
+
+while True:
+    if player.collide(goal):
+        goal.write('WIN',font=('Arial',25,'bold'))
+        
+        goal.hideturtle()
+        enemy1.hideturtle()
+        enemy2.hideturtle()
+        player.hideturtle()
+
+    if player.collide(enemy1) or player.collide(enemy2):
+        enemy2.write('LOSE',font=('Arial',25,'bold'))
+
+        goal.hideturtle()
+        enemy1.hideturtle()
+        enemy2.hideturtle()
+        player.hideturtle()
+
 
 
 
