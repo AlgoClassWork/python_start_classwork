3bfe58961f6a19f9f2eb6fca2f0ddb9226a70363
AlgoClassWork <kyxec.play@gmail.com>
2024-07-01 11:03:10 +0600

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -1,11 +1,20 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget)
+    QApplication, QWidget,
+    QLabel, QLineEdit,
+    QPushButton, QVBoxLayout)
 
 #ИНТЕРФЕЙС
 app = QApplication([])
 
 window = QWidget()
+window.setWindowTitle('Игра Угадай Число')
+window.resize(600,400)
 
+text = QLabel('Попробуй победить меня я загадал число от 1 до 100')
+#РАСПОЛОЖЕНИЕ
+main_line = QVBoxLayout()
+main_line.addWidget(text)
+window.setLayout(main_line)
 #ЗАПУСК
 window.show()
 app.exec()
