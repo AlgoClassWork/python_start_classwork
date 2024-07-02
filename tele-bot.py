92dacb1816bf81c104940f87118616c3c41cf5ec
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 11:03:51 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -15,12 +15,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'Что бы вы хотели? \n'
     )
 
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
+    text = update.message.text.lower()
+
+    if 'шут' in text:
+         await update.message.reply_text('Колобок повесился')
+
 
 def main():
     application = Application.builder().token("Ваш токен").build()
 
     application.add_handler(CommandHandler("start", start))
-    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     application.run_polling()
 
