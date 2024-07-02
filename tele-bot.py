8d53d5acc47d7e2ba98ef644dbe8c844ecd0a080
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 10:41:47 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -3,11 +3,24 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 from random import randint
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Здравствуйте меня зовут бот Генадий. \n'
+        'Вот что я умею делать: \n'
+        '- Шутить \n'
+        '- Рекомендация оняме  \n'
+        '- Сыграть во что нибудь \n'
+        '- Могу продать вам хлам \n'
+        '\n'
+        'Что бы вы хотели? \n'
+    )
+
+
 def main():
-    application = Application.builder().token("ВАШ ТОКЕН").build()
+    application = Application.builder().token("Ваш токен").build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     application.run_polling()
 
