1713101573edc73cad80a0b403ad0c0d6258cc4f
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 14:49:34 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -1,9 +1,14 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-        
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Здравствуй человек я сверх продвинутый исскуственный ынтылект \n'
+        'Давай пообщаемся на различные высоко ынтелектуальные темы'
+    )
+  
 def main():
-    application = Application.builder().token("ТВОЙ ТОКЕН").build()
+    application = Application.builder().token("СЮДА ВСТАВЬТЕ ВАШ ТОКЕН").build()
 
     application.add_handler(CommandHandler("start", start))
     #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
