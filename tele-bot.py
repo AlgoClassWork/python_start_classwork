4e12011349503967a9474062a00eb7f4dd8c574d
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 15:27:55 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -10,10 +10,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.lower()
     if 'привет' in text:
-        await update.message.reply_text('ты красавчик')
-  
+        await update.message.reply_text('Привет чува(к-иха)')
+    elif 'как дела' in text:
+        await update.message.reply_text('Нормально как у тебя?')
+    elif 'норм' in text:
+        await update.message.reply_text('Я тебя поздравляю')
+    elif 'фильм' in text:
+        await update.message.reply_text('Какой жанр?')
+        context.user_data['awaiting_genre'] = True
+    elif context.user_data.get('awaiting_genre'):
+        genre = text
+        if 'комедия' in genre:
+            await update.message.reply_text('Мальчишник в вегасе')
+        elif 'ужас' in genre:
+            await update.message.reply_text('Пила')
+        context.user_data.pop('awaiting_genre')
+    else:
+        await update.message.reply_text('Извини я тебя не понимаю')
+
 def main():
-    application = Application.builder().token("ВАШ ТОКЕН").build()
+    application = Application.builder().token("7180974589:AAEWa3rIqZc3i8WeBjwhGxOPY5JV4Mf3jdE").build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
