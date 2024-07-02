04d1d343f1d2b0b001bd672d1239ef8f8485e032
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 16:19:04 +0600

    Create maze.py

---
+++ maze.py
@@ -0,0 +1,17 @@
+from pygame import *
+
+window = display.set_mode((700,500))
+display.set_caption('ИГРА ЛАБИРИНТ')
+
+background =  transform.scale(image.load('background.jpg'),(700,500))
+
+game = True
+while game:
+
+    window.blit(background,(0,0))
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
