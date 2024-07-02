f5ee8dbcf2f767c1de0ec7e3e72c12799bd365dc
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 14:02:19 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -1,62 +1,13 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from random import randint
 
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        'Здравствуйте меня зовут бот Генадий. \n'
-        'Вот что я умею делать: \n'
-        '- Шутить \n'
-        '- Рекомендация оняме  \n'
-        '- Сыграть во что нибудь \n'
-        '- Могу продать вам хлам \n'
-        '\n'
-        'Что бы вы хотели? \n'
-    )
-
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
-    text = update.message.text.lower()
-
-    if 'шут' in text:
-         await update.message.reply_text('Колобок повесился')
-    elif 'аним' in text:
-         await update.message.reply_text('Введите жанр:')
-         context.user_data['awaiting_genre'] = True
-    elif context.user_data.get('awaiting_genre'):
-        genre = text
-        if genre == 'приключение':
-            await update.message.reply_text('Ван пис')
-        elif genre == 'экшен':
-            await update.message.reply_text('Атака титаника')
-        else:
-            await update.message.reply_text('Таких жанров я пока не знаю')
-        context.user_data.pop('awaiting_genre')
-
-    elif 'фильм' in text:
-         await update.message.reply_text('Введите жанр:')
-         context.user_data['awaiting_genre_film'] = True
-    elif context.user_data.get('awaiting_genre_film'):
-        genre = text
-        if genre == 'приключение':
-            await update.message.reply_text('Индиана джонс')
-        elif genre == 'экшен':
-            await update.message.reply_text('Форсаж 666')
-        else:
-            await update.message.reply_text('Таких жанров я пока не знаю')
-        context.user_data.pop('awaiting_genre_film')
-    else:
-        await update.message.reply_text('Я тебя не понимаю')
         
-
 def main():
-    application = Application.builder().token("7148594036:AAG-hcT4eLgSjaR1EFVeRUKyonhomaR-C5A").build()
+    application = Application.builder().token("ТВОЙ ТОКЕН").build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     application.run_polling()
-
 
 main()
