ae576b7b127a6f598ee62e6797638d3f545a8731
AlgoClassWork <kyxec.play@gmail.com>
2024-07-02 15:07:27 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -6,12 +6,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'Здравствуй человек я сверх продвинутый исскуственный ынтылект \n'
         'Давай пообщаемся на различные высоко ынтелектуальные темы'
     )
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower()
+    if 'привет' in text:
+        await update.message.reply_text('ты красавчик')
   
 def main():
-    application = Application.builder().token("СЮДА ВСТАВЬТЕ ВАШ ТОКЕН").build()
+    application = Application.builder().token("ВАШ ТОКЕН").build()
 
     application.add_handler(CommandHandler("start", start))
-    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     application.run_polling()
 
