2b180b3483e326bfc4276e96a4e5c931e2273cb3
AlgoClassWork <kyxec.play@gmail.com>
2024-07-04 17:51:34 +0600

    Update shooter.py

--- shooter.py
+++ shooter.py
@@ -26,20 +26,24 @@ class Player(GameSprite):
         bullets.add(bullet)
 
 class Enemy(GameSprite):
-
     def update(self):
+        global lost
         self.rect.y += self.speed
         if self.rect.y > 500:
             self.rect.y = 0
             self.rect.x = randint(0,600)
+            lost += 1
 
 class Bullet(GameSprite):
-
     def update(self):
         self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
 
 player = Player(sprite_img='rocket.png',cord_x=300,cord_y=400,width=70,height=100,speed=15)
+
 bullets = sprite.Group()
+
 enemys = sprite.Group()
 for _ in range(5):
     enemy = Enemy(sprite_img='ufo.png',cord_x=randint(0,600),cord_y=0,width=100,height=50,speed=randint(1,3)) 
@@ -53,9 +57,18 @@ background =  transform.scale(image.load('galaxy.jpg'),(700,500))
 #mixer.init()
 #mixer.music.load('music.mp3')
 #mixer.music.play()
+#fire_sound = mixer.Sound('fire.mp3')
+
+font.init()
+font1 = font.Font(None,35)
+font2 = font.Font(None,120)
+
+score = 0
+lost = 0
 
 clock = time.Clock()
 
+finish = False
 game = True
 
 while game:
@@ -66,19 +79,42 @@ while game:
 
         elif e.type == KEYDOWN:
             if e.key == K_SPACE:
+                #fire_sound.play()
                 player.fire()
 
-    window.blit(background,(0,0))
+    if finish != True:
 
-    player.reset()
-    player.move()
+        window.blit(background,(0,0))
 
-    enemys.draw(window)
-    enemys.update()
+        player.reset()
+        player.move()
 
-    bullets.draw(window)
-    bullets.update()
+        enemys.draw(window)
+        enemys.update()
 
+        bullets.draw(window)
+        bullets.update()
 
-    clock.tick(30)
+        score_label = font1.render('Убито:' + str(score),1,(255,255,0))
+        lost_label = font1.render('Пропущено:' + str(lost),1,(255,255,0))
+
+        window.blit(score_label,(10,10))
+        window.blit(lost_label,(10,40))
+
+        if sprite.groupcollide(enemys,bullets,True,True):
+            score += 1
+            enemy = Enemy(sprite_img='ufo.png',cord_x=randint(0,600),cord_y=0,width=100,height=50,speed=randint(1,3)) 
+            enemys.add(enemy)
+
+        if lost > 5:
+            lose = font2.render('ТЫ ПРОИГРАЛ',1,(255,0,0))
+            window.blit(lose,(100,250))
+            finish = True
+
+        if score > 15:
+            win = font2.render('ТЫ ПОБЕДИЛ',1,(0,255,0))
+            window.blit(win,(100,250))
+            finish = True
+
+    clock.tick(60)
     display.update()
