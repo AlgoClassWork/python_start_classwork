77888deb7e40cf61eef3e1540523f007f19de2ef
AlgoClassWork <kyxec.play@gmail.com>
2024-07-15 14:59:25 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,47 +1,15 @@
-database = {
-    'Гейб Ньюэл': {
-        'должность': 'директор',
-        'эффективность': 10,
-        'проекты': ['dota 2','cs-go 2','half-life 2']
-    },
-    'Илон Маск': {
-        'должность': 'гений',
-        'эффективность': 30,
-        'проекты': ['tesla','space-x','pay-pal']
-    },
-    'Павел Вольнадуров': {
-        'должность': 'телеграмм шутник',
-        'эффективность': 30,
-        'проекты': ['telegram','comedy-club','V-Odnoklassnikah']
-    },
-}
+trainings = {
 
-question = input('Что бы хотели узнать о наших сотрудниках?')
-while question != 'off':
-    if question == 'фамилии':
-        print('Фамилии сотрудников:')
-        for surname in database:
-            print('-',surname.split()[1])
-    elif question == 'эффективность':
-        effectivnost = []
-        for surname in database:
-            effectivnost.append(database[surname]['эффективность'])
-        effectivnost.sort()
-        max_effect = effectivnost[-1]
-        samyi_krutoi = ''
-        for surname in database:
-            if database[surname]['эффективность'] == max_effect:
-                samyi_krutoi = surname
-        print('Самый эффективный сотрудник:', samyi_krutoi)
-    elif question == 'должности':
-        print('Должности наших сотрудников:')
-        for surname in database:
-            print(surname.split()[0],'-',database[surname]['должность'])
-    elif question == 'проекты':
-        name = input('Последний проект какого сотрудника вас интересует')
-        for surname in database:
-            if name == surname.split()[0]:
-                print('Последний проект:',database[surname]['проекты'][-1])
+    'Онбординг':{
+        'ответственный':'Ершов В.С.',
+        'темы': ['техника безопасности', 'работа в команде'],
+        'дата': '15.05'
+    },
 
-    question = input('Что бы хотели узнать о наших сотрудниках?')
+    'Повышение квалификации':{
+        'ответственный':'Мишин Н.В.',
+        'темы': ['лидерство', 'компьютерная грамотность'],
+        'дата': '20.11'
+    },
     
+}
