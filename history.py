40f1c2f1e7dc8b522c50d63eb69de5ef894b00c2
AlgoClassWork <kyxec.play@gmail.com>
2024-07-22 15:23:42 +0600

    Update history.py

--- history.py
+++ history.py
@@ -1,3 +1,24 @@
+from time import *
+
+students = open('students.txt','r',encoding='utf-8')
+
+total_iq = 0
+people = 0
+
+for student in students:
+    student_info = student.split() 
+    student_name = student_info[0]
+    student_surname = student_info[1]
+    student_iq = int(student_info[2])
+
+    total_iq += student_iq
+    people += 1
+
+print('Средний iq балл исторической личности равен:', total_iq/people)
+
+
+
+
 Александр Пушкин 120
 Адольф Гитлер 99
 Альберт Эйнштейн 160
