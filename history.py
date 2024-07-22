e571030073f4b9695d516c1c20ec1461644c28a0
AlgoClassWork <kyxec.play@gmail.com>
2024-07-22 15:37:23 +0600

    Update history.py

--- history.py
+++ history.py
@@ -4,10 +4,14 @@ students = open('students.txt','r',encoding='utf-8')
 
 total_iq = 0
 people = 0
+
 genius = []
 stupid = []
 
+start = time() 
+
 for student in students:
+
     student_info = student.split() 
     student_name = student_info[0]
     student_surname = student_info[1]
@@ -21,6 +25,9 @@ for student in students:
     if student_iq < 100:
         stupid.append(student_surname)
 
+end = time() 
+
 print('Средний iq балл исторической личности равен:', total_iq/people)
-print('Список гениев на экране:', genius)
-print('Список дурачков на экране:', stupid)
+print('Список гениев на экране:', len(genius))
+print('Список дурачков на экране:', len(stupid))
+print('Анализ данных занял', end-start, 'с')
