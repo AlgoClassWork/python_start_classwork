bbe8545c6cf48543a82e1e1f037dbad80344065a
AlgoClassWork <kyxec.play@gmail.com>
2024-07-23 14:32:11 +0600

    Update zametki.py

--- zametki.py
+++ zametki.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QTextEdit, QHBoxLayout)
+    QApplication, QWidget, QTextEdit, QHBoxLayout,
+    QLabel, QListWidget, QPushButton, QVBoxLayout)
 
 # ОБЬЕКТЫ ИНТЕРФЕЙСА
 app = QApplication([])
@@ -7,6 +8,11 @@ app = QApplication([])
 window = QWidget()
 text_field = QTextEdit()
 
+list_notes_text = QLabel('Список заметок:')
+list_notes = QListWidget()
+create_note = QPushButton('Создать')
+delete_note = QPushButton('Удалить')
+save_note = QPushButton('Сохранить')
 # РАЗМЕЩЕНИЕ ОБЬЕКТОВ
 main_layout = QHBoxLayout()
 
