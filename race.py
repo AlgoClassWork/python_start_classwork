6a35f5677ca58b9e564d6d97f9922d4e141cb290
AlgoClassWork <kyxec.play@gmail.com>
2024-07-25 11:17:15 +0600

    Update race.py

--- race.py
+++ race.py
@@ -11,13 +11,18 @@ def start_race(name,col,cord_y):
    name.goto(-200,cord_y)
 
 start_race(name=pica,col='yellow',cord_y=50)
-start_race(name=adolf,col='white',cord_y=-50)
+start_race(name=adolf,col='black',cord_y=-50)
 
 while pica.xcor() < 200 and adolf.xcor() < 200:
    pica.forward( randint(1,20) )
    adolf.forward( randint(1,20) )
 
 if adolf.xcor() > pica.xcor():
-   print('победил адольф')
+   adolf.goto(-200,0)
+   adolf.write('ПОБЕДА АДОЛЬФА',font=('Arial',35,'bold'))
 else:
-   print('победил пикачу')
+   pica.goto(-200,0)
+   pica.write('ПОБЕДА ПИКАЧУ',font=('Arial',35,'bold'))
+
+adolf.hideturtle()
+pica.hideturtle()
