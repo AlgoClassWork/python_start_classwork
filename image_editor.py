27290ffc8bb683780a071324b09a3379ea2c96c0
AlgoClassWork <kyxec.play@gmail.com>
2024-07-26 15:12:42 +0600

    Update image_editor.py

--- image_editor.py
+++ image_editor.py
@@ -9,6 +9,17 @@ window = QWidget()
 window.resize(1000,600)
 window.setWindowTitle('Photoshop')
 
+btn_folder = QPushButton('Папка')
+list_images = QListWidget()
+
+image_field = QLabel('Здесь будет картинка!')
+
+btn_left = QPushButton('Лево')
+btn_right = QPushButton('Право')
+btn_mirror = QPushButton('Зеркало')
+btn_sharpen = QPushButton('Резкость')
+btn_gray = QPushButton('Ч\Б')
+
 # ЗАПУСК
 window.show()
 app.exec()
