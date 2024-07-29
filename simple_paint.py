10c2090cae160fdb720f4c3590356defec47ed7e
AlgoClassWork <kyxec.play@gmail.com>
2024-07-29 11:34:39 +0600

    Update simple_paint.py

--- simple_paint.py
+++ simple_paint.py
@@ -1,11 +1,11 @@
 from turtle import *
-from random import choice #new
+from random import choice 
 #создание обьекта черепашки
 picaso = Turtle()
 picaso.width(5)
 picaso.speed(3)
 picaso.shape('circle')
-picaso.colors = ['red','orange','yellow','green','lightblue','blue','purple'] # new
+picaso.colors = ['red','orange','yellow','green','lightblue','blue','purple'] 
 #создание обьекта экрана
 screen = picaso.getscreen() 
 screen.listen() 
@@ -18,11 +18,27 @@ def move(x,y):
     picaso.goto(x,y)
     picaso.pendown()
 
-def change_color(): #new
+def change_color(): 
     color = choice(picaso.colors) 
     picaso.color(color)
+
+def go_up(): #new
+    picaso.goto(picaso.xcor(),picaso.ycor()+5)
+
+def go_left(): #new
+    picaso.goto(picaso.xcor()-5,picaso.ycor())
+
+def go_down(): #new
+    picaso.goto(picaso.xcor(),picaso.ycor()-5)
+
+def go_right(): #new
+    picaso.goto(picaso.xcor()+5,picaso.ycor())
 
 #подписки на события
 picaso.ondrag(draw)
 screen.onscreenclick(move) 
-screen.onkey(change_color,'c')  #new
+screen.onkey(change_color,'c')  
+screen.onkey(go_up,'w') #new
+screen.onkey(go_left,'a') #new
+screen.onkey(go_down,'s') #new
+screen.onkey(go_right,'d') #new
