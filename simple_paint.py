508ecb613a90375bfcc31c357a8508ebc2c9b50d
AlgoClassWork <kyxec.play@gmail.com>
2024-07-29 11:02:20 +0600

    Update simple_paint.py

--- simple_paint.py
+++ simple_paint.py
@@ -1,19 +1,28 @@
 from turtle import *
 #создание обьекта черепашки
 picaso = Turtle()
-picaso.width(10)
-picaso.speed(10)
+picaso.width(5)
+picaso.speed(3)
 picaso.shape('circle')
 #создание обьекта экрана
-screen = picaso.getscreen() #new
+screen = picaso.getscreen() 
+screen.listen() #new
 #функционал
 def draw(x,y):
     picaso.goto(x,y)
 
-def move(x,y): # new
+def move(x,y): 
     picaso.penup()
     picaso.goto(x,y)
     picaso.pendown()
+
+def set_green(): # new
+    picaso.color('green')
+
+def set_red(): # new
+    picaso.color('red')
 #подписки на события
 picaso.ondrag(draw)
-screen.onscreenclick(move) #new
+screen.onscreenclick(move) 
+screen.onkey(set_green,'g') # new
+screen.onkey(set_red,'r') # new
