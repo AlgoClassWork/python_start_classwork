46ae3dfc75955c781bbbb390e278d084db1ed158
AlgoClassWork <kyxec.play@gmail.com>
2024-07-30 11:36:09 +0600

    Update dragon_quest.py

--- dragon_quest.py
+++ dragon_quest.py
@@ -12,13 +12,31 @@ class Person():
         udar = randint(1,3)
         if udar == 1:
             vrag.health -= self.damage * 2
-            print('Крит урон',self.damage * 2,'у противника осталось',vrag.health,'хп')
+            print('Крит урон у противника осталось',vrag.health,'хп')
         if udar == 2:
             vrag.health -= self.damage
-            print('Атака прошла успешна',self.damage,'у противника осталось',vrag.health,'хп')
+            print('Атака прошла успешна у противника осталось',vrag.health,'хп')
         if udar == 3:
             print('Промах')
+
+    def fight(self,vrag):
+
+        while self.health > 0 and vrag.health > 0:
+            self.punch(vrag)
+            vrag.punch(self)
+
+        if self.health > 0:
+            print(vrag.name,'пал в этом нелегком бою')
+            print('Вы получили усиление теперь ваши статы равны')
+            self.health *= 8
+            self.damage *= 2
+            print('Урон:', self.damage, 'Здоровье:',self.health)
+        else:
+            print('Game over')
+
 
 hero = Person(name='Джон',weapon='Меч',health=100,damage=20)
 enemy = Person(name='Адольф',weapon='Сирюкен',health=90,damage=18)
 dragon = Person(name='Смауг',weapon='Огонь',health=200,damage=40)
+
+
