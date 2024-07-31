b3fe466bfff1d31c6c0f392f71509ed2f4274642
AlgoClassWork <kyxec.play@gmail.com>
2024-07-31 15:20:32 +0600

    Update labirint.py

--- labirint.py
+++ labirint.py
@@ -11,6 +11,7 @@ class GameSprite(sprite.Sprite):
     def show(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
     
+#создание игровых обьектов
 player = GameSprite(img='hero.png',cord_x=0,cord_y=450,width=150,height=150)
 enemy = GameSprite(img='enemy.png',cord_x=800,cord_y=200,width=100,height=150)
 goal = GameSprite(img='goal.png',cord_x=850,cord_y=500,width=150,height=100)
@@ -31,6 +32,26 @@ while game:
     player.show()
     enemy.show()
     goal.show()
+    #движение персонажей
+    keys = key.get_pressed()
+    if keys[K_w] and player.rect.y > 0:
+        player.rect.y -= 5
+    if keys[K_s] and player.rect.y < 450:
+        player.rect.y += 5
+    if keys[K_a] and player.rect.x > 0:
+        player.rect.x -= 5
+    if keys[K_d] and player.rect.x < 850:
+        player.rect.x += 5
+
+    if player.rect.x < enemy.rect.x:
+        enemy.rect.x -= 1
+    else:
+        enemy.rect.x += 1
+
+    if player.rect.y < enemy.rect.y:
+        enemy.rect.y -= 1
+    else:
+        enemy.rect.y += 1
     #обработка нажантия на крестик
     for some_event in event.get():
         if some_event.type == QUIT:
