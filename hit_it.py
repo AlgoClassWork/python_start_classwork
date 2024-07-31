8b0d84c389d5e8c70a9c354e67dbffedbef83fc0
AlgoClassWork <kyxec.play@gmail.com>
2024-07-31 11:07:18 +0600

    Update hit_it.py

--- hit_it.py
+++ hit_it.py
@@ -9,7 +9,16 @@ class Sprite(Turtle):
         self.shape(forma)
         self.left(90)
 
+#игровые обьекты
 player = Sprite(x=0,y=-150,col='yellow',forma='turtle')
 enemy1 = Sprite(x=-150,y=-100,col='red',forma='square')
 enemy2 = Sprite(x=150,y=100,col='red',forma='square')
 goal = Sprite(x=0,y=150,col='green',forma='triangle')
+#обьект экрана
+screen = player.getscreen()
+screen.listen()
+#подписки на события
+screen.onkey(player.move_up(),'w')
+screen.onkey(player.move_down(),'s')
+screen.onkey(player.move_left(),'a')
+screen.onkey(player.move_right(),'d')
