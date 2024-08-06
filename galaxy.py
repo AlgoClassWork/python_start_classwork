514e1e9743c1e7def801f1f1bf2240e75cb9ada5
AlgoClassWork <kyxec.play@gmail.com>
2024-08-06 14:52:34 +0600

    Update galaxy.py

--- galaxy.py
+++ galaxy.py
@@ -1,18 +1,36 @@
 from pygame import *
+from random import *
 
 #класс для добавления персонажей
 class GameSprite(sprite.Sprite):
-    def __init__(self,img,cord_x,cord_y,width,height):
+    def __init__(self,img,cord_x,cord_y,width,height,speed):
         self.image = transform.scale(image.load(img),(width,height))
         self.rect = self.image.get_rect()
         self.rect.x = cord_x
         self.rect.y = cord_y
+        self.speed = speed
 
     def show(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
+class Rocket(GameSprite):
+    def move(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 900:
+            self.rect.x += self.speed
+
+class Enemy(GameSprite):
+    def move(self):
+        self.rect.y += self.speed
+        if self.rect.y > 600:
+            self.rect.y = 0
+            self.rect.x = randint(0,900)
+
 #создание ракеты
-player = GameSprite(img='sheep.png',cord_x=450,cord_y=500,width=100,height=100)
+player = Rocket(img='sheep.png',cord_x=450,cord_y=500,width=100,height=100,speed=5)
+
 
 #создание экрана
 window = display.set_mode((1000,600))
@@ -26,6 +44,8 @@ while game:
     window.blit(back,(0,0))
     #отображение корабля
     player.show()
+    #движение персонажей
+    player.move()
     
     #обработка нажантия на крестик
     for some_event in event.get():
