a0cfea4a856c1a9256ef754de41c0b1ba9ca4412
AlgoClassWork <kyxec.play@gmail.com>
2024-08-07 10:45:46 +0600

    Update simple_game.py

--- simple_game.py
+++ simple_game.py
@@ -11,24 +11,41 @@ pica = transform.scale(pica,(100,100))
 pica_x = 0 #new
 pica_y = 0 #new
 
+enemy = image.load('enemy.png') 
+enemy = transform.scale(enemy,(100,100))  
+
+enemy_x = 0 #new
+enemy_y = 0 #new
+
 game = True
 while game:
 
     window.blit(back_image,(0,0)) 
     window.blit(pica,(pica_x,pica_y))  #update
+    window.blit(enemy,(enemy_x,enemy_y))  #update
 
     keys = key.get_pressed() # new
     if keys[K_w]:
-        pica_y -= 1
+        pica_y -= 5
     if keys[K_s]:
-        pica_y += 1
+        pica_y += 5
     if keys[K_a]:
-        pica_x -= 1
+        pica_x -= 5
     if keys[K_d]:
-        pica_x += 1
+        pica_x += 5
+
+    if pica_x > enemy_x:
+        enemy_x += 1
+    if pica_x < enemy_x:
+        enemy_x -= 1
+    if pica_y > enemy_y:
+        enemy_y += 1
+    if pica_y < enemy_y:
+        enemy_y -= 1
     
     for some_event in event.get():
         if some_event.type == QUIT:
             game = False
 
+    time.delay(30)
     display.update() 
