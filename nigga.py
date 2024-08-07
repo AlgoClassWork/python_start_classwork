09c3ee458a899cd8e15063bb33e2f39c4604fb5a
AlgoClassWork <kyxec.play@gmail.com>
2024-08-07 11:48:06 +0600

    Update nigga.py

--- nigga.py
+++ nigga.py
@@ -1,5 +1,23 @@
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton, QVBoxLayout)
+from random import choice
+
+
+names = [
+    "Белекова Элина",
+    "Исакова Айбийке",
+    "Качкынов эмир",
+    "Кожомбердиев Эльхан",
+    "Лебедев Василий",
+    "Мешанло Эмир",
+    "Самудинова Акылай",
+    "Суюнбаев Эльхан",
+    "Токталыев Руслан",
+    "Токтосунов Нурлан",
+    "Хидернабиев Махьди",
+    "Шарипова Адина",
+    "Кухестани Артем"
+]
 
 #создание виджетов
 app = QApplication([])
@@ -19,6 +37,12 @@ line.addWidget(text)
 line.addWidget(button)
 
 window.setLayout(line)
+#функционал
+def show_nigga():
+    text.setText('Нигга по имени: '+ choice(names))
+#подписка на события
+button.clicked.connect(show_nigga)
+
 #запуск приложения
 window.show()
 app.exec()
