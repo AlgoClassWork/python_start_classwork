189fef7a3d9cdeec21e40bcc46f3cf08b9622faa
AlgoClassWork <kyxec.play@gmail.com>
2024-08-07 14:01:43 +0600

    Update galaxy.py

--- galaxy.py
+++ galaxy.py
@@ -1,67 +1,99 @@
 from pygame import *
-from random import *
+from random import randint
 
-#класс для добавления персонажей
+# фоновая музыка
+
+
+# шрифты и надписи
+
+# нам нужны такие картинки:
+img_back = "back.jpg" # фон игры
+img_hero = "player.png" # герой
+img_enemy = "enemy.png" # враг
+
+# важные переменные
+
+
+# класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
-    def __init__(self,img,cord_x,cord_y,width,height,speed):
-        super().__init__()
-        self.image = transform.scale(image.load(img),(width,height))
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
-        self.rect.x = cord_x
-        self.rect.y = cord_y
-        self.speed = speed
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-    def show(self):
-        window.blit(self.image,(self.rect.x,self.rect.y))
-
-class Rocket(GameSprite):
-    def move(self):
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 0:
+        if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < 900:
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
+  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
 
+
+# класс спрайта-врага   
 class Enemy(GameSprite):
+    # движение врага
     def update(self):
         self.rect.y += self.speed
-        if self.rect.y > 600:
+        # исчезает, если дойдет до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
-            self.rect.x = randint(0,900)
 
-#создание ракеты
-player = Rocket(img='sheep.png',cord_x=450,cord_y=500,width=100,height=100,speed=10)
+# Создаем окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
 
-#создание группы врагов
-enemys = sprite.Group()
-for _ in range(5):
-    enemy = Enemy('enemy.png',randint(0,900),0,100,100,randint(1,3))
-    enemys.add(enemy)
+# создаем спрайты
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
-#создание экрана
-window = display.set_mode((1000,600))
-display.set_caption('ШУТЕР')
-#загрузка изображений
-back = transform.scale(image.load('back.jpg'),(1000,600))
-#создание таймера
-timer = time.Clock()
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
 
-game = True
-while game:
-    #отображение фона
-    window.blit(back,(0,0))
-    #отображение корабля
-    player.show()
-    #движение персонажей
-    player.move()
-    #группа врагов
-    enemys.draw(window)
-    enemys.update()
-    
-    #обработка нажантия на крестик
-    for some_event in event.get():
-        if some_event.type == QUIT:
-            game = False
+# переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+# Основной цикл игры:
+run = True # флаг сбрасывается кнопкой закрытия окна
+while run:
+    # событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
 
-    timer.tick(60)
-    display.update()
+    if not finish:
+        # обновляем фон
+        window.blit(background,(0,0))
+
+        # пишем текст на экране
+
+        # производим движения спрайтов
+        ship.update()
+        monsters.update()
+
+        # обновляем их в новом местоположении при каждой итерации цикла
+        ship.reset()
+        monsters.draw(window)
+
+        display.update()
+    # цикл срабатывает каждую 0.05 секунд
