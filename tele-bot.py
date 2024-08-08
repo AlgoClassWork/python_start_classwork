ed1a86c2810604e8177049f2f1fe169f989aff45
AlgoClassWork <kyxec.play@gmail.com>
2024-08-08 11:24:57 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -3,24 +3,33 @@ from telegram.ext import (
 Application, CommandHandler,
 MessageHandler, filters, ContextTypes)
 
-#обработка команды старт
+# обработка команды старт
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         'ПРИВЕТ БРООООООООО!!! \n' 
         'Отправь /help чтобы получить справку')
     
-#обработка команды help  
+# обработка команды help  
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         'Могу пообщаться с тобой \n'
         'Могу пошутить \n'
-        'Могу порекомендовать фильмы \n')
+        'Могу порекомендовать фильмы')
+
+# обработка всех сообщений которые отправляет пользователь
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower() # получаем текст сообщения пользователя
+    if text == 'привет':
+        await update.message.reply_text('Здравствуйте мой господин')
+    elif text == 'как дела':
+        await update.message.reply_text('Не важно как твои?')
 
 # соединение telegram бота с кодом
 app = Application.builder().token("ВАШ ТОКЕН").build()
 # подписки на команды
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help)) # new
+app.add_handler(CommandHandler("help", help))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 # запуск бота
 app.run_polling()
 
