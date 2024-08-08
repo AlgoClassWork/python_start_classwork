ad283757c31807e343c9c17dba5232eb56fc2755
AlgoClassWork <kyxec.play@gmail.com>
2024-08-08 11:00:20 +0600

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -3,13 +3,24 @@ from telegram.ext import (
 Application, CommandHandler,
 MessageHandler, filters, ContextTypes)
 
+#обработка команды старт
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('ПРИВЕТ БРООООООООО!!!')
+    await update.message.reply_text(
+        'ПРИВЕТ БРООООООООО!!! \n' 
+        'Отправь /help чтобы получить справку')
+    
+#обработка команды help  
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Могу пообщаться с тобой \n'
+        'Могу пошутить \n'
+        'Могу порекомендовать фильмы \n')
 
 # соединение telegram бота с кодом
 app = Application.builder().token("ВАШ ТОКЕН").build()
 # подписки на команды
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help)) # new
 # запуск бота
 app.run_polling()
 
