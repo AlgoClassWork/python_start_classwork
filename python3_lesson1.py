ecc159f4a12f17d54c5061fc8a7c0bcdeebeae14
AlgoClassWork <kyxec.play@gmail.com>
2024-09-15 12:55:24 +0600

    Update python3_lesson1.py

--- python3_lesson1.py
+++ python3_lesson1.py
@@ -7,3 +7,12 @@ while balance > 0 and input('Хотите сделать покупку (да/н
         balance -= price
         print('Сумма', price, 'руб. списана со счёта.')
 print('Доступная сумма:', balance, 'руб.')
+
+
+start = int(input("Год начала сотрудничества с фондом"))
+end = int(input("Год окончания сотрудничества с фондом"))
+for year in range(start, end + 1):
+    if year % 2 == 0:
+        print(year, "год - серия конференций и круглых столов с экспертами.")
+    else:
+        print(year, "год - международный конкурс 'Стартап года'.")
