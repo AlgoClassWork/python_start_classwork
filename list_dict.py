7a8282035be77c1442c2a5c3e6b9901a8c2d8261
AlgoClassWork <kyxec.play@gmail.com>
2024-09-23 17:47:23 +0600

    Update list_dict.py

--- list_dict.py
+++ list_dict.py
@@ -1,4 +1,12 @@
-months = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
-moneys = input('Введите доходы через пробел').split()
-moneys = list(map(int,moneys))
-
+months = 'январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь'.split()
+sales = input('Введите данные о доходах с января по декабрь').split()
+sales = list(map(int, sales))
+growth = []
+fall = []
+for i in range(1, len(sales)):
+    if sales[i] > sales[i-1]:
+        growth.append(months[i])
+    if sales[i] < sales[i-1]:
+        fall.append(months[i])
+print('Месяцы роста доходов:', growth)
+print('Месяцы снижения доходов:', fall)
