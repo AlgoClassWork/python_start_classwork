074bf3419b45d56250242d66bc97ae0fbdc1bb4f
AlgoClassWork <kyxec.play@gmail.com>
2024-09-28 13:38:03 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -2,18 +2,4 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-
-def test():
-    print('Событие!')
-
-class MyApp(App):
-    def build(self):
-        txt = Label(text='Это надпись')
-        btn = Button(text='Это кнопка')
-        btn.on_press = test 
-        layout = BoxLayout()
-        layout.add_widget(txt)
-        layout.add_widget(btn)
-        return layout
-
-MyApp().run()
+from kivy.uix.screenmanager import ScreenManager, Screen
