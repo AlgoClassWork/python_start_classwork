c488d3b8aa5b79a4b40cc7f8534de6b603bd0a27
AlgoClassWork <kyxec.play@gmail.com>
2024-09-28 13:46:59 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -4,6 +4,17 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+class FirstScr(Screen):
+    def __init__(self, name='first'):
+        super().__init__(name=name)
+        btn = Button(text="Переключиться на другой экран")
+        btn.on_press = self.next
+        self.add_widget(btn)
+
+    def next(self):
+        self.manager.transition.direction = 'left' 
+        self.manager.current = 'second'
+
 class MyApp(App):
     def build(self):
         return
