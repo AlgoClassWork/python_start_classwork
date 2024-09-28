b7221b4086410fc8114f3e1323c99b0d9b99f5b1
AlgoClassWork <kyxec.play@gmail.com>
2024-09-28 13:40:26 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -3,3 +3,10 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MyApp(App):
+    def build(self):
+        return
+
+app = MyApp()
+app.run()
