c8f87f0562af4fecf4ea072a68bd795f81317049
AlgoClassWork <kyxec.play@gmail.com>
2024-09-28 10:44:20 +0600

    Update otdel.py

--- otdel.py
+++ otdel.py
@@ -1,7 +1,7 @@
 otdel  = {
     'Васильев' : {
         'должность': 'уборщик',
-        'эффективность' : 40,
+        'эффективность' : 100,
         'портфолио': ['dota2','csgo']
     },
     'Петров' : {
@@ -9,7 +9,21 @@ otdel  = {
         'эффективность': 78,
         'портфолио': ['osu','minecraft']
     },
+    'Байден' : {
+        'должность': 'министр',
+        'эффективность': 90,
+        'портфолио': ['osu','minecraft']
+    },
 }
 
 print('Фамилии всех сотрудников:',list(otdel.keys()))
+sotrudnik = ''
+max_effect = 0
+for surname in otdel:
+    if otdel[surname]['эффективность'] > max_effect:
+        max_effect = otdel[surname]['эффективность']
+        sotrudnik = surname
+print('Самый эффективный',sotrudnik)
+for surname in otdel:
+    print('Должность',surname,'-',otdel[surname]['должность'])
 
