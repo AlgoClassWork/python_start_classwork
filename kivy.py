59dd8bda2231832485c10c7de6c3c8e75e3cc43a
AlgoClassWork <kyxec.play@gmail.com>
2024-09-28 13:57:48 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -7,17 +7,39 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 class FirstScr(Screen):
     def __init__(self, name='first'):
         super().__init__(name=name)
+        box = BoxLayout()
         btn = Button(text="Переключиться на другой экран")
         btn.on_press = self.next
-        self.add_widget(btn)
+        txt = Label(text="Первый экран")
+        box.add_widget(txt)
+        box.add_widget(btn)
+        self.add_widget(box)
 
     def next(self):
         self.manager.transition.direction = 'left' 
         self.manager.current = 'second'
 
+class SecondScr(Screen):
+    def __init__(self, name='second'):
+        super().__init__(name=name)
+        box = BoxLayout()
+        btn = Button(text="Переключиться на другой экран")
+        btn.on_press = self.next
+        txt = Label(text="Второй экран")
+        box.add_widget(btn)
+        box.add_widget(txt)
+        self.add_widget(box)
+
+    def next(self):
+        self.manager.transition.direction = 'right' 
+        self.manager.current = 'first'
+
 class MyApp(App):
     def build(self):
-        return
+        screens = ScreenManager()
+        screens.add_widget(FirstScr())
+        screens.add_widget(SecondScr())
+        return screens
 
 app = MyApp()
 app.run()
