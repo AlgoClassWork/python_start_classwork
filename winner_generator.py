b6532d0951a0bea833e5e892e69ea0f092bf151a
AlgoClassWork <kyxec.play@gmail.com>
2024-10-05 10:53:25 +0600

    Update winner_generator.py

--- winner_generator.py
+++ winner_generator.py
@@ -1,4 +1,5 @@
 # pip install pyqt5
+import random
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 )
@@ -6,17 +7,24 @@ from PyQt5.QtWidgets import (
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Генератор победителя')
-
 text = QLabel('Нажми что бы узнать кто негр?')
-text.setStyleSheet('color:red ; font-size:30px')
 button = QPushButton('Сгенерировать')
+# СТИЛИЗАЦИЯ ЭЛЕМЕНТОВ
+text.setStyleSheet('color:red ; font-size:30px')
 button.setStyleSheet('background-color:lightgreen;font-size:30px')
 # РАЗМЕЩЕНИЕ ЭЛЕМЕНТОВ ИНТЕРФЕЙСА
 line = QVBoxLayout()
 line.addWidget(text)
 line.addWidget(button)
 window.setLayout(line)
-
+# ФУНКЦИОНАЛ
+def show_negr():
+    names = ['Вася','Петя','Эльмир']
+    name = names[random.randint(0,2)]
+    text.setText('Негр по имени: ' + name)
+# ПОДПИСКИ НА СОБЫТИЯ
+button.clicked.connect(show_negr)
+# ЗАПУСК
 window.show()
 app.exec()
 
