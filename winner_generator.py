8bbd0a13c36c57054496682e4e297c5619200dfa
AlgoClassWork <kyxec.play@gmail.com>
2024-10-05 10:40:57 +0600

    Update winner_generator.py

--- winner_generator.py
+++ winner_generator.py
@@ -2,9 +2,20 @@
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 )
-
+# ЭЛЕМЕНТЫ ИНТЕРФЕЙСА
 app = QApplication([])
 window = QWidget()
+window.setWindowTitle('Генератор победителя')
+
+text = QLabel('Нажми что бы узнать кто негр?')
+text.setStyleSheet('color:red ; font-size:30px')
+button = QPushButton('Сгенерировать')
+button.setStyleSheet('background-color:lightgreen;font-size:30px')
+# РАЗМЕЩЕНИЕ ЭЛЕМЕНТОВ ИНТЕРФЕЙСА
+line = QVBoxLayout()
+line.addWidget(text)
+line.addWidget(button)
+window.setLayout(line)
 
 window.show()
 app.exec()
