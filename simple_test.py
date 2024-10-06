48ccc01e030a8b76d7da9ee5fd35344daff493b1
AlgoClassWork <kyxec.play@gmail.com>
2024-10-06 15:57:01 +0600

    Update simple_test.py

--- simple_test.py
+++ simple_test.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget , QLabel, QVBoxLayout, QRadioButton
+    QApplication, QWidget , QLabel, QVBoxLayout, QRadioButton,
+    QHBoxLayout
 )
 # ИНТЕРФЕЙС
 app = QApplication([])
@@ -17,11 +18,15 @@ question_text.setStyleSheet('font-size:30px')
 window.setStyleSheet('QRadioButton {font-size:30px}')
 # РАЗМЕЩЕНИЕ
 main_line = QVBoxLayout()
+top_btn_line = QHBoxLayout()
+bot_btn_line = QHBoxLayout()
 main_line.addWidget(question_text)
-main_line.addWidget(button1)
-main_line.addWidget(button2)
-main_line.addWidget(button3)
-main_line.addWidget(button4)
+main_line.addLayout(top_btn_line)
+top_btn_line.addWidget(button1)
+top_btn_line.addWidget(button2)
+main_line.addLayout(bot_btn_line)
+bot_btn_line.addWidget(button3)
+bot_btn_line.addWidget(button4)
 window.setLayout(main_line)
 # ЗАПУСК
 app.exec()
