bbcd5dc19dbdcd23c88ae61efba2d75bdeb735da
AlgoClassWork <kyxec.play@gmail.com>
2024-10-14 15:34:42 +0600

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,9 +1,16 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QLabel, QVBoxLayout
 )
-
+# ИНТЕРФЕЙС
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Тест')
 window.show()
+
+question_label = QLabel('Какого цвета чернокожие?')
+# РАЗМЕЩЕНИЕ
+main_layout = QVBoxLayout()
+main_layout.addWidget(question_label)
+window.setLayout(main_layout)
+# ЗАПУСК
 app.exec()
