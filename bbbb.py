9769901ee644d19e274f1a29d7c4675931203542
AlgoClassWork <kyxec.play@gmail.com>
2024-10-16 15:26:04 +0600

    Update bbbb.py

--- bbbb.py
+++ bbbb.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import (
     QApplication, QWidget,
     QLabel, QPushButton,
-    QVBoxLayout)
+    QVBoxLayout, QGroupBox)
 
 #ИНТЕРФЕЙС
 app = QApplication([])
@@ -11,10 +11,20 @@ window.show()
 
 question_label = QLabel('Как переводится слово Apple?')
 answer_button = QPushButton('Ответить')
+#СОЗДАНИЕ ФОРМЫ С РЕЗУЛЬТАТОМ
+result_form = QGroupBox('Результат:') 
+result_label = QLabel('Правильно')
+correct_label = QLabel('яблоко')
+result_layout = QVBoxLayout()
+result_layout.addWidget(result_label)
+result_layout.addWidget(correct_label)
+result_form.setLayout(result_layout)
 #РАЗМЕЩЕНИЕ НА ГЛАВНОМ ЭКРАНЕ
 main_layout = QVBoxLayout()
 main_layout.addWidget(question_label)
+main_layout.addWidget(result_form) 
 main_layout.addWidget(answer_button)
 window.setLayout(main_layout)
 #ЗАПУСК
+result_form.hide()
 app.exec()
