68bd127566676e7daaf38ed11c411bc1412e05ae
AlgoClassWork <kyxec.play@gmail.com>
2024-10-16 15:10:20 +0600

    Update bbbb.py

--- bbbb.py
+++ bbbb.py
@@ -1,10 +1,20 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
-)
+    QApplication, QWidget,
+    QLabel, QPushButton,
+    QVBoxLayout)
 
+#ИНТЕРФЕЙС
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Memory card')
 window.show()
 
+question_label = QLabel('Как переводится слово Apple?')
+answer_button = QPushButton('Ответить')
+#РАЗМЕЩЕНИЕ НА ГЛАВНОМ ЭКРАНЕ
+main_layout = QVBoxLayout()
+main_layout.addWidget(question_label)
+main_layout.addWidget(answer_button)
+window.setLayout(main_layout)
+#ЗАПУСК
 app.exec()
