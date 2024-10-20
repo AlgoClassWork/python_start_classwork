71f47840bf2223e07d3e7f41f75397eb8a53fa2e
AlgoClassWork <kyxec.play@gmail.com>
2024-10-20 15:54:46 +0600

    Update txt_practice.py

--- txt_practice.py
+++ txt_practice.py
@@ -19,3 +19,18 @@ while answer != 'нет':
 Джон Сноу 140 180
 Питер Паркер 145 178
 Микки Маус 100 70
+
+
+file = open('person.txt','r',encoding='utf-8')
+heroes = file.readlines()
+amount_hero = len(heroes)
+total_height = 0
+for hero in heroes: 
+    hero = hero.split()  
+    name = hero[0]
+    surname = hero[1]
+    iq = int(hero[2])
+    height = int(hero[3])
+    total_height += height
+
+print('Средний рост персонажа',total_height/amount_hero)
