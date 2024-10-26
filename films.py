652bb080445f800530208d297d00d326a99ef11d
AlgoClassWork <kyxec.play@gmail.com>
2024-10-26 09:50:13 +0600

    Update films.py

--- films.py
+++ films.py
@@ -1,10 +1,15 @@
 from PyQt5.QtWidgets import (
-    QApplication, QWidget
+    QApplication, QWidget, QTextEdit, QHBoxLayout
 )
-
+# ИНТЕРФЕЙС
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Фильмы')
-
+text_field = QTextEdit()
+# РАЗМЕЩЕНИЕ
+main_line = QHBoxLayout()
+main_line.addWidget(text_field)
+window.setLayout(main_line)
+# ЗАПУСК
 window.show()
 app.exec()
