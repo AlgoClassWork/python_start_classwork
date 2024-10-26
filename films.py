8237064fb543313d8d72d12071421cbd66d861b7
AlgoClassWork <kyxec.play@gmail.com>
2024-10-26 10:18:59 +0600

    Update films.py

--- films.py
+++ films.py
@@ -12,6 +12,13 @@ list_films = QListWidget()
 create_film = QPushButton('Добавить фильм')
 delete_film = QPushButton('Удалить фильм')
 save_film = QPushButton('Сохранить фильм')
+
+list_genres = QListWidget()
+search_field = QLineEdit()
+search_field.setPlaceholderText('Введите жанр: ')
+create_genre = QPushButton('Добавить жанр')
+delete_genre = QPushButton('Удалить жанр')
+search_button = QPushButton('Найти фильм по жанру')
 # РАЗМЕЩЕНИЕ
 main_line = QHBoxLayout()
 main_line.addWidget(text_field)
@@ -23,6 +30,12 @@ list_line.addWidget(list_films)
 list_line.addWidget(create_film)
 list_line.addWidget(delete_film)
 list_line.addWidget(save_film)
+
+list_line.addWidget(list_genres)
+list_line.addWidget(search_field)
+list_line.addWidget(create_genre)
+list_line.addWidget(delete_genre)
+list_line.addWidget(search_button)
 
 window.setLayout(main_line)
 # ЗАПУСК
