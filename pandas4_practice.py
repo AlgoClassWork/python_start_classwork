551e6f1f1c9a1ea6afda1192ce10ab2c19ae5742
AlgoClassWork <kyxec.play@gmail.com>
2024-10-30 17:40:24 +0600

    Update pandas4_practice.py

--- pandas4_practice.py
+++ pandas4_practice.py
@@ -1,10 +1,7 @@
 import pandas as pd
 df = pd.read_csv('GooglePlayStore_wild.csv')
 
-
-# Очистка данных из первого задания
 df['Rating'].fillna(-1, inplace = True)
-
 
 def set_size(size):
   if size[-1] == 'M':
@@ -14,16 +11,13 @@ def set_size(size):
   return -1
 df['Size'] = df['Size'].apply(set_size)
 
-
 def set_installs(installs):
   if installs == '0':
       return 0
   return int(installs[:-1].replace(',', ''))
 df['Installs'] = df['Installs'].apply(set_installs)
 
-
 df['Type'].fillna('Free', inplace = True)
-
 
 # Замени тип данных на дробное число (float) для цен приложений ('Price')
 def make_price(price):
@@ -31,3 +25,33 @@ def make_price(price):
      return float(price[1:])
  return 0
 df['Price'] = df['Price'].apply(make_price)
+
+# Вычисли, сколько долларов разработчики заработали на каждом платном приложении
+df['Profit'] = df['Installs'] * df['Price']
+# Чему равен максимальный доход ('Profit') среди платных приложений (Type == 'Paid')?
+print( df['Profit'].max() )
+# Создай новый столбец, в котором будет храниться количество жанров. Назови его 'Number of genres'
+def split_genres(genres):
+   return genres.split(';')
+
+df['Genres'] = df['Genres'].apply(split_genres)
+df['Number of genres'] = df['Genres'].apply(len)
+# Какое максимальное количество жанров ('Number of genres') хранится в датасете?
+print(df['Number of genres'].max())
+# Бонусное задание
+def set_season(date):
+    month = date.split()[0] 
+    seasons = {
+      'Зима' : ['December', 'January', 'February'],
+      'Весна' : ['March', 'April', 'May'],
+      'Лето' : ['June', 'July', 'August'],
+      'Осень': ['September', 'October', 'November']
+   }
+    for season in seasons:
+        if month in seasons[season]:
+           return season
+    return 'Сезон не установлен'
+
+df['Season'] = df['Last Updated'].apply(set_season)
+# Выведи на экран сезоны и их количество в датасете
+print(df['Season'].value_counts())
