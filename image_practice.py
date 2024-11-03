697ada355a59919dec74fe24ad68075537d5ef18
AlgoClassWork <kyxec.play@gmail.com>
2024-11-03 10:42:20 +0600

    Update image_practice.py

--- image_practice.py
+++ image_practice.py
@@ -1,26 +1,25 @@
 from PIL import Image, ImageOps, ImageEnhance
 
-with Image.open('негр.jpg') as picture:
-    # ИНФОРМАЦИЯ О КАРТИНКЕ
-    print('Размер картинки', picture.size)
-    print('Формат картинки', picture.format)
-    print('Тип', picture.mode)
-    #picture.show()
-    # Ч.Б ФИЛЬТР
-    gray_picture = ImageOps.grayscale(picture)
-    print('Тип', gray_picture.mode)
-    #gray_picture.show()
-    # ПОВОРОТ
-    rotate_picture = picture.rotate(180)
-    #rotate_picture.show()
-    # ЗЕРКАЛО
-    mirror_picture = ImageOps.mirror(picture)
-    #mirror_picture.show()
-    contrast_picture = ImageEnhance.Contrast(picture)
-    contrast_picture = contrast_picture.enhance(10)
-    #contrast_picture.show()
-    # СОХРАНЕНИЕ
-    gray_picture.save('серый негр.jpg')
-    rotate_picture.save('повернутый негр.jpg')
-    mirror_picture.save('зеркальный негр.jpg')
-    contrast_picture.save('негр в контрасте.jpg')
+class ImageEditor():
+    def __init__(self, filename):
+        self.filename = filename 
+        self.original = Image.open(self.filename)
+        self.changes = [] 
+        
+    def do_black(self):
+        black = ImageOps.grayscale(self.original)
+        self.changes.append(black)
+        temp = self.filename.split('.') 
+        new = temp[0] + str(len(self.changes)) + '.jpg'
+        black.save(new)
+
+    def flip(self):
+        flip = self.original.rotate(180)
+        self.changes.append(flip)
+        temp = self.filename.split('.') 
+        new = temp[0] + str(len(self.changes)) + '.jpg'
+        flip.save(new)
+
+image = ImageEditor('негр.jpg')
+image.flip()
+image.do_black()
