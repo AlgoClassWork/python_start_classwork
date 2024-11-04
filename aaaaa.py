7c690d4850f676b34d0d97048e7e73eee96c2aa2
AlgoClassWork <kyxec.play@gmail.com>
2024-11-04 17:41:14 +0600

    Update aaaaa.py

--- aaaaa.py
+++ aaaaa.py
@@ -3,7 +3,17 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('GoogleApps.csv')
 
-size_info = data['Size']
-size_info.plot(kind='hist')
+# Гистограмма
+#size_info = data['Size']
+#size_info.plot(kind='hist')
+# Ящик с усами 
+#paid = data[data['Type'] == 'Free']['Rating']
+#paid.plot(kind='box')
+# Диаграма Рассеяния
+#data.plot(x = 'Installs', y = 'Size', kind='scatter')
+# Круговая диаграма
+#category = data['Category'].value_counts().nlargest(10)
+#content = data['Content Rating'].value_counts()
+#category.plot(kind='pie')
 
 plt.show()
