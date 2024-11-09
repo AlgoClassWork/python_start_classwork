97fd2d87746209d61c007dccbe5dec93333c642f
AlgoClassWork <kyxec.play@gmail.com>
2024-11-09 15:36:41 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,8 +1,4 @@
 # шаг 1 импорты нужных технологий
-# шаг 2 создаем пустое окно
-# шаг 3 создаем нужные обьекты (виджеты)
-    # создать левую часть интерфейса
-    # создать правую часть интерфейса
 from PyQt5.QtWidgets import (
     QApplication, QWidget,
     QPushButton, QListWidget, QLabel,
@@ -11,10 +7,10 @@ from PyQt5.QtWidgets import (
 # шаг 2 создаем пустое окно
 app = QApplication([])
 window = QWidget()
-
+# создать левую часть интерфейса
 folder_btn = QPushButton('Папка')
 images_list = QListWidget()
-
+# создать правую часть интерфейса
 image_label = QLabel('Здесь будет картинка')
 left_btn = QPushButton('Лево')
 right_btn = QPushButton('Право')
@@ -23,7 +19,30 @@ sharp_btn = QPushButton('Резкость')
 bw_btn = QPushButton('Ч\Б')
 save_btn = QPushButton('Сохранить')
 reset_btn = QPushButton('Сбросить')
-
-
+ # создать линии
+main_line = QHBoxLayout()
+left_line = QVBoxLayout()
+right_line = QVBoxLayout()
+btn_line = QHBoxLayout()
+# размещение нужных частей на главную линию
+main_line.addLayout(left_line)
+main_line.addLayout(right_line)
+# размещение виджетов левой части интерфейса
+left_line.addWidget(folder_btn)
+left_line.addWidget(images_list)
+# размещение виджетов правой части интерфейса
+right_line.addWidget(image_label)
+right_line.addLayout(btn_line)
+# размещение кнопок на линию для кнопок
+btn_line.addWidget(left_btn)
+btn_line.addWidget(right_btn)
+btn_line.addWidget(mirror_btn)
+btn_line.addWidget(sharp_btn)
+btn_line.addWidget(bw_btn)
+btn_line.addWidget(save_btn)
+btn_line.addWidget(reset_btn)
+# разместить главную линию на экране
+window.setLayout(main_line)
+# запуск приложения
 window.show()
 app.exec()
