116ed78fa4f42d82288951a81f80132ed7dda6c3
AlgoClassWork <kyxec.play@gmail.com>
2024-11-09 10:11:27 +0600

    Update photoshop.py

--- photoshop.py
+++ photoshop.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import (
     QVBoxLayout, QHBoxLayout,
     QPushButton, QListWidget, QLabel)
 
+# ИНТЕРФЕЙС
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Фотошоп')
@@ -19,5 +20,30 @@ btn_bw = QPushButton('Ч\Б')
 btn_save = QPushButton('Сохранить')
 btn_reset = QPushButton('Сбросить')
 
+# РАЗМЕЩЕНИЕ
+main_line = QHBoxLayout()
+left_line = QVBoxLayout()
+right_line = QVBoxLayout()
+btn_line = QHBoxLayout()
+
+main_line.addLayout(left_line,20)
+main_line.addLayout(right_line,80)
+
+left_line.addWidget(btn_folder)
+left_line.addWidget(list_files)
+
+right_line.addWidget(label_image)
+right_line.addLayout(btn_line)
+
+btn_line.addWidget(btn_left)
+btn_line.addWidget(btn_right)
+btn_line.addWidget(btn_mirror)
+btn_line.addWidget(btn_sharp)
+btn_line.addWidget(btn_bw)
+btn_line.addWidget(btn_save)
+btn_line.addWidget(btn_reset)
+
+# ЗАПУСК
+window.setLayout(main_line)
 window.show()
 app.exec()
