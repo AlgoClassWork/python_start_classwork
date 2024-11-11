66566cbb65e320acc1b193af228ea4dd452236d2
AlgoClassWork <kyxec.play@gmail.com>
2024-11-11 16:15:02 +0600

    Update photo.py

--- photo.py
+++ photo.py
@@ -19,6 +19,32 @@ btn_bw = QPushButton('Ч\Б')
 btn_save = QPushButton('Сохранить')
 btn_reset = QPushButton('Сбросить')
 
+# РАЗМЕЩЕНИЕ ЭЛЕМЕНТОВ ИНТЕРФЕЙСА
+main_line = QHBoxLayout()
+left_line = QVBoxLayout()
+right_line = QVBoxLayout()
+buttons_line = QHBoxLayout()
+
+main_line.addLayout(left_line, 20)
+main_line.addLayout(right_line, 80)
+
+left_line.addWidget(btn_folder)
+left_line.addWidget(list_images)
+
+right_line.addWidget(label_image)
+right_line.addLayout(buttons_line)
+
+buttons_line.addWidget(btn_left)
+buttons_line.addWidget(btn_right)
+buttons_line.addWidget(btn_mirror)
+buttons_line.addWidget(btn_sharp)
+buttons_line.addWidget(btn_bw)
+buttons_line.addWidget(btn_save)
+buttons_line.addWidget(btn_reset)
+
 # ЗАПУСК ПРИЛОЖЕНИЯ
+window.setWindowTitle('Фотожоб')
+window.setLayout(main_line)
+window.resize(800,500)
 window.show()
 app.exec()
