5604a8531a91e67aa9a2e342eeadec098cbaa1f6
AlgoClassWork <kyxec.play@gmail.com>
2024-11-16 12:36:17 +0600

    Create titanik.py

---
+++ titanik.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+
+
+# Шаг 1. Загрузка и очистка данных
+df = pd.read_csv('titanic.csv')
+df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1, inplace = True)
+df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
+df['Embarked'].fillna('S', inplace = True)
+df.drop('Embarked', axis = 1, inplace = True)
+
+
+age_1 = df[df['Pclass'] == 1]['Age'].median()
+age_2 = df[df['Pclass'] == 2]['Age'].median()
+age_3 = df[df['Pclass'] == 3]['Age'].median()
+
+
+def fill_age(row):
+   if pd.isnull(row['Age']):
+       if row['Pclass'] == 1:
+           return age_1
+       if row['Pclass'] == 2:
+           return age_2
+       return age_3
+   return row['Age']
+
+
+df['Age'] = df.apply(fill_age, axis = 1)
