f2eb7989fe930c294d5abd0f1502aae5d6fd0b35
AlgoClassWork <kyxec.play@gmail.com>
2024-11-23 13:18:20 +0600

    Update mine-game.py

--- mine-game.py
+++ mine-game.py
@@ -2,15 +2,15 @@
 
 
 from direct.showbase.ShowBase import ShowBase
+
 from mapmanager import Mapmanager
 
-
+    
 class Game(ShowBase):
-   def __init__(self):
-       ShowBase.__init__(self)
-       self.land = Mapmanager()
-       base.camLens.setFov(90)
-
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager()
+        self.land.loadLand('land.txt')
 
 game = Game()
 game.run()
@@ -20,27 +20,44 @@ game.run()
 
 
 class Mapmanager():
-   """ Управление картой """
-   def __init__(self):
-       self.model = 'block' # модель кубика лежит в файле block.egg
-       # # используются следующие текстуры:
-       self.texture = 'block.png'        
-       self.color = (0.2, 0.2, 0.35, 1) #rgba
-
-
-       # создаём основной узел карты:
-       self.startNew()
+    """ Управление картой """
+    def __init__(self):
+        self.model = 'block' # модель кубика лежит в файле block.egg
+        # # используются следующие текстуры: 
+        self.texture = 'block.png'
         # создаём строительные блоки   
-       self.addBlock((0,10, 0))
+        self.block = loader.loadModel(self.model)
+        self.block.setTexture(loader.loadTexture(self.texture))           
+        self.color = (0.2, 0.2, 0.35, 1) #rgba
+
+        # создаём основной узел карты:
+        self.startNew() 
+        self.addBlock((0,10, 0))
+
+    def startNew(self):
+        """создаёт основу для новой карты""" 
+        self.land = render.attachNewNode("Land") # узел, к которому привязаны все блоки карты
+    
+    def addBlock(self, position):
+        self.block = loader.loadModel(self.model)
+        self.block.setTexture(loader.loadTexture(self.texture)) 
+        self.block.setPos(position)
+        self.block.setColor(self.color)
+        self.block.reparentTo(self.land)
+
+    def loadLand(self, filename):
+        # загружаем данные из текстового файла
+        land = open(filename)
+        # алгоритм загрузки карты из числовой модели
+        y = 0
+        for block_line in land:
+            x = 0
+            block_line = block_line.split()
+            for block_count in block_line:
+                for z in range( int(block_count) + 1 ):
+                    block = self.addBlock((x,y,z))
+                x += 1
+            y += 1
 
 
-   def startNew(self):
-       """создаёт основу для новой карты"""
-       self.land = render.attachNewNode("Land") # узел, к которому привязаны все блоки карты
-  
-   def addBlock(self, position):
-       self.block = loader.loadModel(self.model)
-       self.block.setTexture(loader.loadTexture(self.texture))
-       self.block.setPos(position)
-       self.block.setColor(self.color)
-       self.block.reparentTo(self.land)
+
