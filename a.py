5acf0ca0df956c837c72232b373642726cc6dd26
AlgoClassWork <kyxec.play@gmail.com>
2024-11-23 15:35:10 +0600

    Update a.py

--- a.py
+++ a.py
@@ -5,11 +5,16 @@ display.set_caption('Догонялки')
 #загрузка игровых картинок
 background = image.load('back.png')
 background = transform.scale(background, (700,500))
+
+pica = transform.scale(image.load('pica.png'), (100,100))
+gita = transform.scale(image.load('gita.png'), (100,100))
 #запускаем игровой цикл
 game = True
 while game:
     #отображение картинок
     window.blit(background, (0,0) )
+    window.blit(pica, (100,100) )
+    window.blit(gita, (300,100) )
     #обрабатываем нажатие на крестик
     for e in event.get():
         if e.type == QUIT:
