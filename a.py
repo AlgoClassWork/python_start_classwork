ce493ea5b1a53df3ddcea1e181e7ace2849f91b5
AlgoClassWork <kyxec.play@gmail.com>
2024-11-24 15:46:10 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,23 @@
 from pygame import *
 
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, img, x, y):
+        self.image =  transform.scale(image.load(img), (70,70))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self):
+        window.blit(self.image, (self.rect.x,self.rect.y))
+
+hero = GameSprite('hero.png',200,200)
+enemy = GameSprite('cyborg.png',400,200)
+
 # Игровая сцена
 window = display.set_mode((700,500))
 display.set_caption('Лабиринт ужаса')
-back = transform.scale(image.load('background3.png'),(700,500))
+back = transform.scale(image.load('background.jpg'),(700,500))
 
 # Музыка
 #mixer.init()
@@ -21,6 +35,9 @@ while game:
             game = False
 
     window.blit(back, (0,0))
+
+    hero.draw()
+    enemy.draw()
 
     display.update()
     clock.tick(60)
