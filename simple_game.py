8d8c2a1b84ad21cba97bcea76b550a05eb5302e4
AlgoClassWork <kyxec.play@gmail.com>
2024-11-27 15:38:36 +0600

    Update simple_game.py

--- simple_game.py
+++ simple_game.py
@@ -3,8 +3,11 @@ from pygame import *
 window = display.set_mode( (700,500) )
 display.set_caption('Догонялки')
 
-background = image.load('фон.jpg')
+background = image.load('фон.png')
 background = transform.scale(background, (700,500) )
+
+pica = transform.scale(image.load('pica.png'), (100,100) )
+gita = transform.scale(image.load('gita.png'), (100,100) )
 
 game = True
 while game:
@@ -13,6 +16,9 @@ while game:
         if some_event.type == QUIT:
             game = False
 
+    
     window.blit(background, (0,0) )
+    window.blit(pica, (200,200))
+    window.blit(gita, (400,200))
 
     display.update()
