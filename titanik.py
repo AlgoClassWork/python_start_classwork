9cd4ac8e5b3f8a7db1a473b4394c02b1f2829a11
AlgoClassWork <kyxec.play@gmail.com>
2024-11-27 17:50:40 +0600

    Update titanik.py

--- titanik.py
+++ titanik.py
@@ -24,4 +24,22 @@ def fill_gender(gender):
 
 data['Sex'] = data['Sex'].apply(fill_gender)
 
-print(data.info())
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+x = data.drop('Survived', axis = 1)
+y = data['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+standart = StandardScaler()
+x_train = standart.fit_transform(x_train)
+x_test = standart.transform(x_test)
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+print('Процент правильных предсказаний:', accuracy_score(y_test, y_pred) * 100 )
