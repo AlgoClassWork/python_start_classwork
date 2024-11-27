d191cad9a3f51e0cf78c98ca0b3966025bac211d
AlgoClassWork <kyxec.play@gmail.com>
2024-11-27 16:14:34 +0600

    Update simple_game.py

--- simple_game.py
+++ simple_game.py
@@ -9,6 +9,11 @@ background = transform.scale(background, (700,500) )
 pica = transform.scale(image.load('pica.png'), (100,100) )
 gita = transform.scale(image.load('gita.png'), (100,100) )
 
+pica_x, pica_y = 200, 200
+gita_x, gita_y = 400, 200
+
+clock = time.Clock()
+
 game = True
 while game:
 
@@ -16,9 +21,20 @@ while game:
         if some_event.type == QUIT:
             game = False
 
-    
     window.blit(background, (0,0) )
-    window.blit(pica, (200,200))
-    window.blit(gita, (400,200))
+    window.blit(pica, (pica_x, pica_y))
+    window.blit(gita, (gita_x, gita_y))
 
+    keys = key.get_pressed()
+
+    if keys[K_w] and pica_y > 0:
+        pica_y -= 3
+    if keys[K_s] and pica_y < 400:
+        pica_y += 3
+    if keys[K_a] and pica_x > 0:
+        pica_x -= 3
+    if keys[K_d] and pica_x < 600:
+        pica_x += 3
+    
+    clock.tick(60)
     display.update()
