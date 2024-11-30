76fb1d600168443508dc280d4b929e8738867811
AlgoClassWork <kyxec.play@gmail.com>
2024-11-30 17:28:28 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,84 +1,9 @@
-from pygame import *
-'''Необходимые классы'''
-
-
-#класс-родитель для спрайтов
-class GameSprite(sprite.Sprite):
-   #конструктор класса
-   def __init__(self, player_image, player_x, player_y, player_speed):
-       # каждый спрайт должен хранить свойство image - изображение
-       self.image = transform.scale(image.load(player_image), (65, 65))
-       self.speed = player_speed
-       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
-
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
-
-class Player(GameSprite):
-    def move(self):
-        keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 435:
-            self.rect.y += self.speed
-        if keys[K_a] and self.rect.x > 0:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < 635:
-            self.rect.x += self.speed
-
-class Enemy(GameSprite):
-    direct = 'left'
-    def move(self):
-        if self.rect.x > 635:
-            self.direct = 'left'
-        if self.rect.x < 300:
-            self.direct = 'right'
-
-        if self.direct == 'left':
-            self.rect.x -= self.speed
-        if self.direct == 'right':
-            self.rect.x += self.speed
-
-#Игровая сцена:
-win_width = 700
-win_height = 500
-window = display.set_mode((win_width, win_height))
-display.set_caption("Maze")
-background = transform.scale(image.load("background.jpg"), (win_width, win_height))
-
-
-#Персонажи игры:
-player = Player('hero.png', 5, win_height - 80, 4)
-monster = Enemy('cyborg.png', win_width - 80, 280, 2)
-final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
-
-
-game = True
-clock = time.Clock()
-FPS = 60
-
-
-#музыка
-#mixer.init()
-#mixer.music.load('jungles.ogg')
-#mixer.music.play()
-
-
-while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
-  
-   window.blit(background,(0, 0))
-   player.reset()
-   monster.reset()
-
-   player.move()
-   monster.move()
-
-   display.update()
-   clock.tick(FPS)
-
+sales = input('Желаете изучить хиты продаж?')
+if sales == 'да':
+    category = input('Введите категорию:')
+    if category == 'продукты':
+        print('Молоко 1л, Печенье с изюмом, Персики')
+    else:
+        print('Стиральный порошок, Щётка для обуви')
+else:
+    print('Дайте знать, если передумаете!')
