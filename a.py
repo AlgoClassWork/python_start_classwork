18a2a2063d8e108eb8cd5ca52adc72876a4474a8
AlgoClassWork <kyxec.play@gmail.com>
2024-11-30 14:23:18 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,59 +1,23 @@
 from pygame import *
 '''Необходимые классы'''
 
-#класс-родитель для спрайтов 
+
+#класс-родитель для спрайтов
 class GameSprite(sprite.Sprite):
-    #конструктор класса
-    def __init__(self, player_image, player_x, player_y, player_speed):
-        super().__init__()
- 
-        # каждый спрайт должен хранить свойство image - изображение
-        self.image = transform.scale(image.load(player_image), (65, 65))
-        self.speed = player_speed
- 
-        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
+   #конструктор класса
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (65, 65))
+       self.speed = player_speed
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
 
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
-    def move(self):
-        keys =  key.get_pressed()
-        if keys[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 65:
-            self.rect.y += self.speed
-        if keys[K_a] and self.rect.x > 0:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < win_width - 65:
-            self.rect.x += self.speed
-
-class Enemy(GameSprite):
-    direction = 'left'
-
-    def move(self):
-        if self.rect.x <= 400:
-            self.direction = 'right'
-        if self.rect.x >= win_width - 65:
-            self.direction = 'left'
-
-        if self.direction == 'left':
-            self.rect.x -= self.speed
-        if self.direction == 'right':
-            self.rect.x += self.speed
-
-    def move2(self, player):
-        if self.rect.x > player.rect.x:
-            self.rect.x -= self.speed
-        if self.rect.x < player.rect.x:
-            self.rect.x += self.speed
-        if self.rect.y > player.rect.y:
-            self.rect.y -= self.speed
-        if self.rect.y < player.rect.y:
-            self.rect.y += self.speed
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
 
 
 #Игровая сцена:
@@ -63,34 +27,33 @@ window = display.set_mode((win_width, win_height))
 display.set_caption("Maze")
 background = transform.scale(image.load("background.jpg"), (win_width, win_height))
 
+
 #Персонажи игры:
-player = Player('hero.png', 5, win_height - 80, 4)
-monster = Enemy('cyborg.png', win_width - 80, 280, 2)
-monster2 = Enemy('cyborg.png', win_width - 80, 280, 2)
+player = GameSprite('hero.png', 5, win_height - 80, 4)
+monster = GameSprite('cyborg.png', win_width - 80, 280, 2)
 final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+
 
 game = True
 clock = time.Clock()
 FPS = 60
+
 
 #музыка
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
 
+
 while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    
-    window.blit(background,(0, 0))
-    player.reset()
-    monster.reset()
-    monster2.reset()
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+  
+   window.blit(background,(0, 0))
+   player.reset()
+   monster.reset()
 
-    player.move()
-    monster.move()
-    monster2.move2(player)
 
-    display.update()
-    clock.tick(FPS)
+   display.update()
+   clock.tick(FPS)
