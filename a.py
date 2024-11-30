5faa449c39cf4f3a0b7ce925eab047db2908c63c
AlgoClassWork <kyxec.play@gmail.com>
2024-11-30 17:50:55 +0600

    Update a.py

--- a.py
+++ a.py
@@ -7,3 +7,21 @@ if sales == 'да':
         print('Стиральный порошок, Щётка для обуви')
 else:
     print('Дайте знать, если передумаете!')
+
+
+price1 = int(input('Цена первого товара')) 
+price2 = int(input('Цена второго товара')) 
+price3 = int(input('Цена третьего товара')) 
+
+if price1 >= price2:
+    if price1 >= price3:
+        max_price = price1
+    else:
+        max_price = price3
+else:
+    if price2 >= price3:
+        max_price = price2
+    else:
+        max_price = price3
+
+print('Акция! К оплате за три товара:', max_price)
