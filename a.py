f02e1dcdd1ae84a6f8a1fc35c493d1f45e245b02
AlgoClassWork <kyxec.play@gmail.com>
2024-12-02 16:16:46 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,152 +1,37 @@
 from pygame import *
-'''Необходимые классы'''
 
+class GameSprite():
+    def __init__(self, img, x, y):
+        self.image = transform.scale(image.load(img),(70,70))
+        self.x = x
+        self.y = y
 
-#класс-родитель для спрайтов
-class GameSprite(sprite.Sprite):
-   #конструктор класса
-   def __init__(self, player_image, player_x, player_y, player_speed):
-       super().__init__()
-       # каждый спрайт должен хранить свойство image - изображение
-       self.image = transform.scale(image.load(player_image), (65, 65))
-       self.speed = player_speed
-       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
+    def show(self):
+        window.blit(self.image,(self.x,self.y))
 
+hero = GameSprite(img='hero.png', x=200, y=200)
+enemy = GameSprite(img='cyborg.png', x=400, y=200)
 
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
+window = display.set_mode( (700,500) )
+display.set_caption('Лабиринт')
 
-
-#класс-наследник для спрайта-игрока (управляется стрелками)
-class Player(GameSprite):
-   def update(self):
-       keys = key.get_pressed()
-       if keys[K_LEFT] and self.rect.x > 5:
-           self.rect.x -= self.speed
-       if keys[K_RIGHT] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-
-
-#класс-наследник для спрайта-врага (перемещается сам)
-class Enemy(GameSprite):
-   direction = "left"
-   def update(self):
-       if self.rect.x <= 200:
-           self.direction = "right"
-       if self.rect.x >= win_width - 85:
-           self.direction = "left"
-
-
-       if self.direction == "left":
-           self.rect.x -= self.speed
-       else:
-           self.rect.x += self.speed
-
-class Wall(sprite.Sprite):
-    def __init__(self, wall_x, wall_y, wall_width, wall_height):
-        self.image = Surface( (wall_width, wall_height) )
-        self.image.fill( (100,250,100) )
-        self.rect = self.image.get_rect()
-        self.rect.x = wall_x
-        self.rect.y = wall_y
-
-    def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
-
-    direction = "left"
-    def update(self, start, end):
-       if self.rect.x <= start:
-           self.direction = "right"
-       if self.rect.x >= end:
-           self.direction = "left"
-
-       if self.direction == "left":
-           self.rect.x -= 2
-       else:
-           self.rect.x += 2
-
-#Игровая сцена:
-win_width = 700
-win_height = 500
-window = display.set_mode((win_width, win_height))
-display.set_caption("Maze")
-background = transform.scale(image.load("background.jpg"), (win_width, win_height))
-
-
-#Персонажи игры:
-player = Player('hero.png', 5, win_height - 80, 4)
-monster = Enemy('cyborg.png', win_width - 80, 280, 10)
-final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
-
-wall_1 = Wall(10,10,650,10)
-wall_2 = Wall(200,100,10,400)
-wall_3 = Wall(300,100,400,10)
-wall_4 = Wall(300,400,400,10)
-
-walls = [wall_1, wall_2, wall_3, wall_4]
-
-
-game = True
-finish = False
-clock = time.Clock()
-FPS = 60
-
+background = transform.scale(image.load('background.jpg'), (700,500))
 
 #музыка
 #mixer.init()
 #mixer.music.load('jungles.ogg')
 #mixer.music.play()
 
-#надписи
-font.init()
-my_font = font.Font(None,150)
-win_text = my_font.render('YOU WIN!',True,(0,255,0))
-lose_text = my_font.render('YOU LOSE!',True,(255,0,0))
-
-
+clock = time.Clock()
+game = True
 while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
-  
-   if finish != True:
-       window.blit(background,(0, 0))
-       player.update()
-       monster.update()
-       wall_2.update(100,400)
-      
-       player.reset()
-       monster.reset()
-       final.reset()
+    for some_event in event.get():
+        if some_event.type == QUIT:
+            game = False
 
-       wall_1.reset()
-       wall_2.reset()
-       wall_3.reset()
-       wall_4.reset()
+    window.blit(background, (0, 0))
+    hero.show()
+    enemy.show()
 
-
-       if sprite.collide_rect(player, final):
-           window.blit(win_text,(100,200))
-           finish = True
-
-       if sprite.collide_rect(player, monster):
-           window.blit(lose_text,(100,200))
-           finish = True
-
-       for wall in walls:
-           if sprite.collide_rect(player, wall):
-               player.rect.x = 20
-               player.rect.y = 420
-
-     
-
-
-   display.update()
-   clock.tick(FPS)
+    display.update()
+    clock.tick(60)
