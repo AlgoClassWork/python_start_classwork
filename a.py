799f655b1ec331819944f9c5aba606e3d4d60607
AlgoClassWork <kyxec.play@gmail.com>
2024-12-04 16:17:58 +0600

    Update a.py

--- a.py
+++ a.py
@@ -13,17 +13,31 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def move(self):
         keys = key.get_pressed()
-        if keys[K_w]:
+        if keys[K_w] and self.rect.y > 0 :
             self.rect.y -= 5
-        if keys[K_s]:
+        if keys[K_s] and self.rect.y < 430:
             self.rect.y += 5
-        if keys[K_a]:
+        if keys[K_a] and self.rect.x > 0:
             self.rect.x -= 5
-        if keys[K_d]:
+        if keys[K_d] and self.rect.x < 630:
             self.rect.x += 5
-    
+
+class Enemy(GameSprite):
+    direction = 'left'
+    def move(self, point_a, point_b):
+        if self.rect.x > point_b:
+            self.direction = 'left'
+        if self.rect.x < point_a:
+            self.direction = 'right'
+
+        if self.direction == 'left':
+            self.rect.x -= 5
+        else:
+            self.rect.x += 5
+
 hero = Player(img='hero.png', x=200, y=200)
-enemy = GameSprite(img='cyborg.png', x=400, y=200)
+enemy = Enemy(img='cyborg.png', x=400, y=200)
+enemy2 = Enemy(img='cyborg.png', x=600, y=400)
 
 window = display.set_mode( (700,500) )
 display.set_caption('Лабиринт')
@@ -45,8 +59,11 @@ while game:
     window.blit(background, (0, 0))
     hero.show()
     enemy.show()
+    enemy2.show()
 
     hero.move()
+    enemy.move(0,300)
+    enemy2.move(300,600)
 
     if sprite.collide_rect(hero, enemy):
         print('Столкновение')
