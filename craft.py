f54bc586f62821fa036457bf7b41ddc41a56bf08
AlgoClassWork <kyxec.play@gmail.com>
2024-12-04 17:13:55 +0600

    Update craft.py

--- craft.py
+++ craft.py
@@ -5,6 +5,7 @@ class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.land = Mapmanager()
+        self.land.loadLand('land.txt')
         base.camLens.setFov(90)
 
 game = Game()
@@ -12,11 +13,10 @@ game.run()
 
 
 
+
 class Mapmanager():
     def __init__(self):
         self.startNew()
-        self.addBlock((0, 10, 0))
-        self.addBlock((1, 10, 0))
 
     def startNew(self):
         self.land = render.attachNewNode('Land')
@@ -26,3 +26,15 @@ class Mapmanager():
         self.block.setTexture(loader.loadTexture('block.png'))
         self.block.setPos(position)
         self.block.reparentTo(self.land)
+
+    def loadLand(self, filename):
+        map = open(filename)
+        y = 0
+        for line in map:
+            block_line = line.split()
+            x = 0
+            for block in block_line:
+                for z in range(int(block) + 1):
+                    one_block = self.addBlock((x, y, z))
+                x += 1
+            y += 1
