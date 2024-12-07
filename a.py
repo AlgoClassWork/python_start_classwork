8c60437a58af42de03a48beec6f6aac0d21fa0bf
AlgoClassWork <kyxec.play@gmail.com>
2024-12-07 15:29:40 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,24 @@
 from pygame import *
 
+# Базовый класс
+class GameSprite():
+    def __init__(self, img, w, h, x, y):
+        self.image = transform.scale(image.load(img), (w,h))
+        self.x = x
+        self.y = y
+
+    def show(self):
+        window.blit(self.image, (self.x,self.y)) 
+
+player = GameSprite(img='rocket.png',w=70,h=100,x=300,y=400)
+enemy = GameSprite(img='ufo.png',w=100,h=70,x=300,y=200)
+
+
 # Создание и настройка экрана
 window = display.set_mode((700,500))
 display.set_caption('Шутер')
 back = transform.scale(image.load('galaxy.jpg'), (700,500))
+
 # Игровые переменные
 game = True
 # Игровой цикл
@@ -14,5 +29,7 @@ while game:
             game = False
     # Отображение обьектов
     window.blit(back, (0,0))
-
+    player.show()
+    enemy.show()
+   
     display.update()
