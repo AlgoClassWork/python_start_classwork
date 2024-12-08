0c9074a4f25e418c5d8bceb8ac1b3bdd164453ce
AlgoClassWork <kyxec.play@gmail.com>
2024-12-08 09:24:50 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,45 +1,90 @@
 from pygame import *
 
-# Базовый класс
-class GameSprite():
-    def __init__(self, img, w, h, x, y):
-        self.image = transform.scale(image.load(img), (w,h))
-        self.x = x
-        self.y = y
 
-    def show(self):
-        window.blit(self.image, (self.x,self.y)) 
+#фоновая музыка
+#mixer.init()
+#mixer.music.load('space.ogg')
+#mixer.music.play()
+#fire_sound = mixer.Sound('fire.ogg')
 
+
+#нам нужны такие картинки:
+img_back = "galaxy.jpg" #фон игры
+img_hero = "rocket.png" #герой
+
+
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #Вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+
+
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+
+
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+#класс главного игрока
 class Player(GameSprite):
-    def move(self):
-        keys = key.get_pressed()
-        if keys[K_a] and self.x > 0:
-            self.x -= 5
-        if keys[K_d] and self.x < 630:
-            self.x += 5
+   #метод для управления спрайтом стрелками клавиатуры
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+       if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+ #метод "выстрел" (используем место игрока, чтобы создать там пулю)
+   def fire(self):
+       pass
 
-player = Player(img='rocket.png',w=70,h=100,x=300,y=400)
-enemy = GameSprite(img='ufo.png',w=100,h=70,x=300,y=200)
+
+#Создаем окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
 
 
-# Создание и настройка экрана
-window = display.set_mode((700,500))
-display.set_caption('Шутер')
-back = transform.scale(image.load('galaxy.jpg'), (700,500))
+#создаем спрайты
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
-# Игровые переменные
-game = True
-# Игровой цикл
-while game:
-    # Обработка выхода из игры
-    for some_event in event.get():
-        if some_event.type == QUIT:
-            game = False
-    # Отображение обьектов
-    window.blit(back, (0,0))
-    player.show()
-    enemy.show()
-    # Передвижение обьектов
-    player.move()
-   
-    display.update()
+
+#переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+#Основной цикл игры:
+run = True #флаг сбрасывается кнопкой закрытия окна
+while run:
+   #событие нажатия на кнопку Закрыть
+   for e in event.get():
+       if e.type == QUIT:
+           run = False
+
+
+   if not finish:
+       #обновляем фон
+       window.blit(background,(0,0))
+
+
+       #производим движения спрайтов
+       ship.update()
+
+
+       #обновляем их в новом местоположении при каждой итерации цикла
+       ship.reset()
+
+
+       display.update()
+   #цикл срабатывает каждые 0.05 секунд
+   time.delay(50)
