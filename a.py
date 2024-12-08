719a2b3b0d0168c66c960733b8d5d47a87a423c0
AlgoClassWork <kyxec.play@gmail.com>
2024-12-08 15:47:20 +0600

    Update a.py

--- a.py
+++ a.py
@@ -8,11 +8,16 @@ from pygame import *
 #mixer.music.play()
 #fire_sound = mixer.Sound('fire.ogg')
 
+score = 0
+lost = 0
+
+font.init()
+my_font = font.Font(None,40)
+
 
 #нам нужны такие картинки:
 img_back = "galaxy.jpg" #фон игры
 img_hero = "rocket.png" #герой
-
 
 #класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
@@ -51,12 +56,12 @@ class Player(GameSprite):
    
 class Enemy(GameSprite):
     def update(self):
+        global lost
         self.rect.y += self.speed
         if self.rect.y > 500:
             self.rect.y = 0
             self.rect.x = randint(0,600)
-
-
+            lost += 1
 
 #Создаем окошко
 win_width = 700
@@ -67,11 +72,11 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 
 
 #создаем спрайты
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 20)
 
 enemys = sprite.Group()
-for _ in range(10):
-    enemy = Enemy('ufo.png',randint(0,600),0,100,60,5)
+for _ in range(5):
+    enemy = Enemy('ufo.png',randint(0,600),0,100,60,randint(1,5))
     enemys.add(enemy)
 
 #переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
@@ -88,20 +93,17 @@ while run:
    if not finish:
        #обновляем фон
        window.blit(background,(0,0))
+       lost_enemys = my_font.render('Пропущено: ' + str(lost), True, (255,255,255))
+       window.blit(lost_enemys,(0,0))
 
 
        #производим движения спрайтов
        ship.update()
+       enemys.update()
       
-
-
        #обновляем их в новом местоположении при каждой итерации цикла
        ship.reset()
-      
-
-
-
-  
+       enemys.draw(window)
 
        display.update()
    #цикл срабатывает каждые 0.05 секунд
