c79c55cf9ea70dd513bb1dfd083fb473168460f8
AlgoClassWork <kyxec.play@gmail.com>
2024-12-11 15:56:54 +0600

    Update a.py

--- a.py
+++ a.py
@@ -5,9 +5,23 @@ display.set_caption('Космический шутер')
 
 back = transform.scale(image.load('galaxy.jpg'), (700,500))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self,img,x,y,w,h):
+        self.image = transform.scale(image.load(img), (w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+hero = GameSprite(img='rocket.png', w=70, h=100, x=300, y=400)
+
 hero_img = transform.scale(image.load('rocket.png'), (70,100))
 hero_x = 300
 hero_y = 400
+
+
+enemy_img = transform.scale(image.load('ufo.png'), (100,70))
+enemy_x = 300
+enemy_y = 400
 
 game = True
 while game:
@@ -17,7 +31,16 @@ while game:
 
     window.blit(back, (0,0))
     window.blit(hero_img, (hero_x, hero_y))
+    window.blit(enemy_img, (enemy_x, enemy_y))
 
+    keys = key.get_pressed()
+    if keys[K_LEFT]:
+        hero_x -= 5
+    if keys[K_RIGHT]:
+        hero_x += 5
+
+
+    time.delay(10)
     display.update()
     
 
