a300503c05d6d0cfdd4361ec3c7a45d1fc0c2e69
AlgoClassWork <kyxec.play@gmail.com>
2024-12-21 10:39:42 +0600

    Update pinpong.py

--- pinpong.py
+++ pinpong.py
@@ -1,85 +1,33 @@
 from pygame import *
-from random import randint
-
+# Основной игровой класс
 class GameSprite(sprite.Sprite):
-    def __init__(self, sprite_img ,cord_x ,cord_y ,width, height, speed):
-        super().__init__()
-        self.image = transform.scale(image.load(sprite_img),(width,height))
+    def __init__(self, img, w, h, x, y):
+        self.image = transform.scale( image.load(img), (w,h) )
         self.rect = self.image.get_rect()
-        self.rect.x = cord_x
-        self.rect.y = cord_y
-        self.speed = speed
+        self.rect.x = x 
+        self.rect.y = y
 
-    def reset(self):
-        window.blit(self.image,(self.rect.x,self.rect.y))
-
-player = GameSprite('platform.png',0,200,20,200,10)
-enemy = GameSprite('enemy.png',650,200,50,200,10)
-ball = GameSprite('ball.png',300,200,70,70,5)
-
-speed_x = 4
-speed_y = 4
-
-score_player = 0
-score_enemy = 0
-
-font.init()
-font = font.Font(None,35)
-
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+# Создаем игровые обьекты
+player = GameSprite('racket.png', 20, 100, 10, 200)
+ai = GameSprite('racket.png', 20, 100, 670, 200)
+ball = GameSprite('ball.png', 50, 50, 325, 225)
+# Настройки экрана
 window = display.set_mode((700,500))
-display.set_caption('ПЕНГ-ПУНГ')
-
-clock = time.Clock()
-
-finish = False
+display.set_caption('пинг-понг')
+# Игровой цикл
 game = True
-
 while game:
-
+    # Обработка нажатия на крестик
     for e in event.get():
         if e.type == QUIT:
             game = False
+    # Отрисовка игровых обьектов
+    window.fill((255,255,255))
 
-    if finish != True:
+    player.show()
+    ai.show()
+    ball.show()
 
-        window.fill( (255,255,255) )
-
-        player.reset()
-        enemy.reset()
-        ball.reset()
-
-        player_label = font.render('Счет: ' + str(score_player),1,(0,0,0))
-        enemy_label = font.render('Счет: ' + str(score_enemy),1,(0,0,0))
-
-        window.blit(player_label,(10,10))
-        window.blit(enemy_label,(600,10))
-
-        mouse_x, mouse_y = mouse.get_pos() 
-        if player.rect.centery > mouse_y:
-            player.rect.y -= 2
-        else:
-            player.rect.y += 2
-
-        if enemy.rect.centery > ball.rect.centery:
-            enemy.rect.y -= 2
-        else:
-            enemy.rect.y += 2
-
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
-
-        if sprite.collide_rect(ball,player) or sprite.collide_rect(ball,enemy):
-            speed_x *= -1 
-        if ball.rect.y < 0 or ball.rect.y > 430:
-            speed_y *= -1
-
-        if ball.rect.x < 0:
-            score_enemy += 1
-            ball.rect.x = 300
-
-        if ball.rect.x > 630:
-            score_player += 1
-            ball.rect.x = 300
-
-    clock.tick(60)
     display.update()
