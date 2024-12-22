6ad9030cfa1510a1c84c09e7e2ab9a12632399d0
AlgoClassWork <kyxec.play@gmail.com>
2024-12-22 09:50:22 +0600

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,84 +1,42 @@
 from pygame import *
-
-# класс для создания персонажей
+# Основной игровой класс
 class GameSprite(sprite.Sprite):
-    def __init__(self, sprite_image, sprite_x, sprite_y, size_x, size_y):
-        #загружаем картинку
-        self.image = transform.scale(image.load(sprite_image), (size_x, size_y))
-        # хитбокс
+    def __init__(self, img, w, h, x, y):
+        self.image = transform.scale( image.load(img), (w,h) )
         self.rect = self.image.get_rect()
-        self.rect.x = sprite_x
-        self.rect.y = sprite_y
- 
+        self.rect.x = x 
+        self.rect.y = y
+
     def show(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-# создание игровых персонажей
-ball = GameSprite('ball.png',500,300,100,100)
-player = GameSprite('player.png',0,300,100,150)
-enemy = GameSprite('enemy.png',900,300,100,150)
-
-# экран для игры
-display.set_caption("поке-понг")
-window = display.set_mode((1000,600))
-background = transform.scale(image.load('fon.jpg'), (1000, 600))
-
-# скорости мяча
-ball_x = 5
-ball_y = 5
-
-# счет игроков
-player_score = 0
-enemy_score = 0
-
-# шрифты
-font.init()
-game_font = font.Font('game_font.ttf',40)
-the_end = game_font.render('THE END',1,(255,255,255))
-
-# игровой таймер
-fps = time.Clock()
-# игровой цикл
+class Player(GameSprite):
+    def move(self):
+        x , y = mouse.get_pos()
+        self.rect.centery = y
+        
+# Создаем игровые обьекты
+player = Player('racket.png', 20, 100, 10, 200)
+ai = GameSprite('racket.png', 20, 100, 670, 200)
+ball = GameSprite('ball.png', 50, 50, 325, 225)
+# Настройки экрана
+window = display.set_mode((700,500))
+display.set_caption('пинг-понг')
+# Игровой цикл
 game = True
 while game:
-
-    # обработка выхода
-    for eve in event.get():
-        if eve.type == QUIT:
+    # Обработка нажатия на крестик
+    for e in event.get():
+        if e.type == QUIT:
             game = False
+    # Отрисовка игровых обьектов
+    window.fill((255,255,255))
 
-    # отображение фона
-    window.blit(background,(0,0))
-    # отображение счета
-    score_player = game_font.render('Player score: ' + str(player_score),1,(0,0,0))
-    score_enemy = game_font.render('AI score: ' + str(enemy_score),1,(0,0,0))
-    window.blit(score_player,(20,20))
-    window.blit(score_enemy,(700,20))
-
-    # отображение персонажей
-    ball.show()
     player.show()
-    enemy.show()
-    # передвижение мяча
-    ball.rect.x += ball_x
-    ball.rect.y += ball_y
+    ai.show()
+    ball.show()
 
-    if ball.rect.y > 500 or ball.rect.y < 0:
-        ball_y *= -1
+    # Движение обьектов
+    player.move()
 
-    # движение игрока
-    x , mouse_y = mouse.get_pos()
-    player.rect.centery = mouse_y
-    # движение врага
-    if enemy.rect.y < ball.rect.y:
-        enemy.rect.y += 3
-    else:
-        enemy.rect.y -= 3
-
-    # отбивание мяча
-    if sprite.collide_rect(ball,player) or sprite.collide_rect(ball,enemy):
-        ball_x *= -1
-
-    # обновление кадров
-    fps.tick(60)
     display.update()
