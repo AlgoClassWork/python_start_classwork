49ba9bd62a14a2c73471e2f3b80618ac3e2c7ffc
AlgoClassWork <kyxec.play@gmail.com>
2025-01-12 12:18:27 +0600

    Update game.py

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ pica_x, pica_y = 0, 100
 ball = image.load('ball.png') 
 ball = transform.scale(ball, (50,50)) 
 ball_x, ball_y = 300, 150 
+speed_x, speed_y = 1, 1
 # Игровой цикл
 game = True
 while game:
@@ -30,5 +31,13 @@ while game:
         pica_y -= 1
     if keys[K_s] and pica_y < 250:
         pica_y += 1
+
+    ball_x += speed_x
+    ball_y += speed_y
+
+    if ball_x > 650 or ball_x < 0:
+        speed_x *= -1
+    if ball_y > 350 or ball_y < 0:
+        speed_y *= -1
 
     display.update()
