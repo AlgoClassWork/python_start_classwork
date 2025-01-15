784fd05d979fe5dc6f0f05f75e9ab60da3818744
AlgoClassWork <kyxec.play@gmail.com>
2025-01-15 17:47:06 +0600

    Update organs_db.py

--- organs_db.py
+++ organs_db.py
@@ -28,5 +28,28 @@ def add_organ(name, category, description, price, image):
     connection.commit()
     connection.close()
 
+def delete_organ(id):
+    connection = sqlite3.connect('organs.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''DELETE FROM organs WHERE id=? ''', (id))
+
+    connection.commit()
+    connection.close()
+
+def update_organ(name, category, description, price, image, id):
+    connection = sqlite3.connect('organs.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''UPDATE organs SET name=?, category=?, description=?, price=?, image=?
+                   WHERE id=?''', (name, category, description, price, image, id))
+
+    connection.commit()
+    connection.close()
+
 create_database()
 add_organ('Рука','Конечность','Сильная','100','')
+update_organ('Рука','Конечность','Обыкновенная','150','', '1')
+add_organ('Нога','Конечность','Сильная','200','')
+add_organ('Брови','Кожа','Волосотая','10','')
+delete_organ('2')
