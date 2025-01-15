ad1af408fbd2d43b34d62c6d9630aef662f99179
AlgoClassWork <kyxec.play@gmail.com>
2025-01-15 17:16:57 +0600

    Update organs_db.py

--- organs_db.py
+++ organs_db.py
@@ -16,4 +16,17 @@ def create_database():
     connection.commit()
     connection.close()
 
+def add_organ(name, category, description, price, image):
+    connection = sqlite3.connect('organs.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    INSERT INTO organs (name, category, description, price, image)
+    VALUES (?, ?, ?, ?, ?)
+    ''',(name, category, description, price, image))
+
+    connection.commit()
+    connection.close()
+
 create_database()
+add_organ('Рука','Конечность','Сильная','100','')
