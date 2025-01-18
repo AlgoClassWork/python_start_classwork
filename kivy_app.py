21e4255605c88fdf03251e622c97e43a09790e85
AlgoClassWork <kyxec.play@gmail.com>
2025-01-18 10:08:41 +0600

    Create kivy_app.py

---
+++ kivy_app.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+#pip install kivy
+
+#Window.clearcolor = (0.95, 0.95, 1, 1)
+
+class FirstScreen(Screen):
+    def __init__(self):
+        pass
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        title = Label(text='Тест', font_size='60px')
+        button = Button(text='Нажми', size_hint=(0.5, 0.2),
+                                      pos_hint={'center_x':0.5},
+                                      background_color=(0,1,0,1))
+        layout.add_widget(title)
+        layout.add_widget(button)
+        return layout
+    
+app = MyApp()
+app.run()
