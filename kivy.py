c606d140553c971a40db2348fc47c8368d3a947a
AlgoClassWork <kyxec.play@gmail.com>
2025-01-18 15:37:54 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -4,13 +4,22 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
+def wrong():
+    print('Сам ты белый')
+
+def correct():
+    print('Теперь ты негр')
+
 
 class MyApp(App):
     def build(self):
         
         label = Label(text='Какого цвета чернокожий?', font_size='60px')
-        button = Button(text='Белый')
-        button2 = Button(text='Розовый')
+        button = Button(text='Белый', size_hint=(0.8, 0.4), color=(1,1,1,1))
+        button2 = Button(text='Черный', size_hint=(0.8, 0.4), color=(0,0,0,1))
+
+        button.on_press = wrong
+        button2.on_press = correct
 
         layout = BoxLayout(orientation='vertical')
         button_layout = BoxLayout()
