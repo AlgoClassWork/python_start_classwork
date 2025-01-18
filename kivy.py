99ad3ed0eecd8c5580eb7fd3ed10a3291972ac26
AlgoClassWork <kyxec.play@gmail.com>
2025-01-18 15:47:03 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -3,23 +3,15 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-
-def wrong():
-    print('Сам ты белый')
-
-def correct():
-    print('Теперь ты негр')
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 
-class MyApp(App):
-    def build(self):
-        
+class FirstScreen(Screen):
+    def __init__(self, name='first'):
+        super().__init__(name=name)
         label = Label(text='Какого цвета чернокожий?', font_size='60px')
         button = Button(text='Белый', size_hint=(0.8, 0.4), color=(1,1,1,1))
         button2 = Button(text='Черный', size_hint=(0.8, 0.4), color=(0,0,0,1))
-
-        button.on_press = wrong
-        button2.on_press = correct
 
         layout = BoxLayout(orientation='vertical')
         button_layout = BoxLayout()
@@ -29,7 +21,14 @@ class MyApp(App):
         button_layout.add_widget(button2)
         layout.add_widget(button_layout)
 
-        return layout
+        self.add_widget(layout)
+
+
+class MyApp(App):
+    def build(self):
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(FirstScreen())
+        return screen_manager
 
 app = MyApp()
 app.run()
