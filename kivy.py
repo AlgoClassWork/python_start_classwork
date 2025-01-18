8e7a8c8df6d0d2e0b055a12d060c61320997ada6
AlgoClassWork <kyxec.play@gmail.com>
2025-01-18 15:24:16 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -1,45 +1,26 @@
+#pip install kivy
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
 
-class FirstScr(Screen):
-    def __init__(self, name='first'):
-        super().__init__(name=name)
-        box = BoxLayout()
-        btn = Button(text="Переключиться на другой экран")
-        btn.on_press = self.next
-        txt = Label(text="Первый экран")
-        box.add_widget(txt)
-        box.add_widget(btn)
-        self.add_widget(box)
-
-    def next(self):
-        self.manager.transition.direction = 'left' 
-        self.manager.current = 'second'
-
-class SecondScr(Screen):
-    def __init__(self, name='second'):
-        super().__init__(name=name)
-        box = BoxLayout()
-        btn = Button(text="Переключиться на другой экран")
-        btn.on_press = self.next
-        txt = Label(text="Второй экран")
-        box.add_widget(btn)
-        box.add_widget(txt)
-        self.add_widget(box)
-
-    def next(self):
-        self.manager.transition.direction = 'right' 
-        self.manager.current = 'first'
 
 class MyApp(App):
     def build(self):
-        screens = ScreenManager()
-        screens.add_widget(FirstScr())
-        screens.add_widget(SecondScr())
-        return screens
+        
+        label = Label(text='Какого цвета чернокожий?', font_size='60px')
+        button = Button(text='Белый')
+        button2 = Button(text='Розовый')
+
+        layout = BoxLayout(orientation='vertical')
+        button_layout = BoxLayout()
+
+        layout.add_widget(label)
+        button_layout.add_widget(button)
+        button_layout.add_widget(button2)
+        layout.add_widget(button_layout)
+
+        return layout
 
 app = MyApp()
 app.run()
