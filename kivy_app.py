08dc5d35fa1d8b41c212bff2a2792b325602f91c
AlgoClassWork <kyxec.play@gmail.com>
2025-01-18 10:36:02 +0600

    Update kivy_app.py

--- kivy_app.py
+++ kivy_app.py
@@ -17,14 +17,37 @@ class FirstScreen(Screen):
                                       background_color=(0,1,0,1))
         layout.add_widget(title)
         layout.add_widget(button)
+
+        button.bind(on_press=self.start_test)
+
+        self.add_widget(layout)
+
+    def start_test(self, instance):
+        app = App.get_running_app()
+        app.sm.current = 'question'
+
+class QuestionScreen(Screen):
+     def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+        question = Label(text='Какого цвета негр?', font_size='60px')
+        answer1 = Button(text='белый')
+        answer2 = Button(text='черный')
+        button_layout = BoxLayout()
+        button_layout.add_widget(answer1)
+        button_layout.add_widget(answer2)
+        layout.add_widget(question)
+        layout.add_widget(button_layout)
         self.add_widget(layout)
 
 
 class MyApp(App):
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(FirstScreen())
-        return sm
+        self.sm = ScreenManager()
+        self.sm.add_widget(FirstScreen(name='first'))
+        self.sm.add_widget(QuestionScreen(name='question'))
+        self.sm.current = 'first'
+        return self.sm
     
 app = MyApp()
 app.run()
