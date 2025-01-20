35351ac521ee4325d57a151db5789ee2b309f739
AlgoClassWork <kyxec.play@gmail.com>
2025-01-20 16:08:32 +0600

    Update kivy.py

--- kivy.py
+++ kivy.py
@@ -5,8 +5,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 class QuestionScreen(Screen):
-    def __init__(self, name):
-        super().__init__()
+    def __init__(self, name='question'):
+        super().__init__(name=name)
         label = Label(text='Какого цвета чернокожие?', font_size='50px')
         button = Button(text='Белый', size_hint=(1,0.5))
         button2 = Button(text='Черный', size_hint=(1,0.5))
@@ -18,10 +18,24 @@ class QuestionScreen(Screen):
         layout.add_widget(layout_button)
         self.add_widget(layout)
 
+class WrongScreen(Screen):
+    def __init__(self, name='wrong'):
+        super().__init__(name=name)
+        label = Label(text='Сам ты белый', font_size='50px')
+        self.add_widget(label)
+
+class CorrectScreen(Screen):
+    def __init__(self, name='correct'):
+        super().__init__(name=name)
+        label = Label(text='Поздравляем теперь ты черный', font_size='50px')
+        self.add_widget(label)
+
 class TestApp(App):
     def build(self):
         screen_manager = ScreenManager()
-        screen_manager.add_widget(QuestionScreen(name='question'))
+        screen_manager.add_widget(QuestionScreen())
+        screen_manager.add_widget(WrongScreen())
+        screen_manager.add_widget(CorrectScreen())
         return screen_manager
 
 app = TestApp()
