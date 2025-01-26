e71da623b3a9b70a0c4a5428603a5c4bdeafddab
AlgoClassWork <kyxec.play@gmail.com>
2025-01-26 11:42:18 +0600

    Update pytelebot.py

--- pytelebot.py
+++ pytelebot.py
@@ -5,11 +5,20 @@ from telegram.ext import Application, CommandHandler,MessageHandler, filters, Co
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text (
-        'Привет Я Чад как поживаешь?'
+        'Привет Я Чад как поживаешь? \n'
+        'Отправь /help чтобы получить справку'
+    )
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text (
+        'Могу пообщаться с тобой \n'
+        'Могу пошутить \n'
+        'Могу порекомендовать фильм'
     )
 
 app = Application.builder().token('7973499836:AAHO_7zjhi4LNDXDxLcbW9_ukDVzbQxQJ9s').build()
 
 app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('help', help))
 
 app.run_polling()
