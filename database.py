6207bd4a87aea1bc7ada510ed6888ad8c2f153ef
AlgoClassWork <kyxec.play@gmail.com>
2025-01-26 15:58:58 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,23 @@
+#index.html
+
+<h1>Список учеников:</h1>
+
+<ul>
+    {% for name in names  %}
+        <li>{{name[1]}} {{name[2]}}</li>
+    {% endfor %}
+</ul>
+
+<h1>Добавить ученика</h1>
+
+<form action="/create" method="post">
+    <input type="text" name="name" placeholder="Имя:">
+    <input type="text" name="surname" placeholder="Фамилия:">
+    <button type="submit">Добавить</button>
+</form>
+
+
+#data.py
 import sqlite3
 
 def create_db():
@@ -29,11 +49,10 @@ def get_name():
 
 
 
-
-
+#main.py
 #pip install flask
-from flask import Flask, render_template
-from data import get_name, create_db
+from flask import Flask, render_template, request, redirect, url_for
+from data import get_name, create_db, add_name
 
 app = Flask(__name__)
 create_db()
@@ -44,24 +63,11 @@ def index():
     names = get_name()
     return render_template('index.html',names=names)
 
+@app.route('/create', methods=['POST'])
+def add():
+    name = request.form.get('name')
+    surname = request.form.get('surname')
+    add_name(name, surname)
+    return redirect(url_for('index'))
+
 app.run()
-
-
-
-
-
-<h1>Список учеников:</h1>
-
-<ul>
-    {% for name in names  %}
-        <li>{{name[1]}} {{name[2]}}</li>
-    {% endfor %}
-</ul>
-
-<h1>Добавить ученика</h1>
-
-<form action="/create" method="post">
-    <input type="text" name="name" placeholder="Имя:">
-    <input type="text" name="surname" placeholder="Фамилия:">
-    <button type="submit">Добавить</button>
-</form>
