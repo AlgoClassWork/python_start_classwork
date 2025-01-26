73b9902cd0a7f528d0139e7ac62a104c346a204c
AlgoClassWork <kyxec.play@gmail.com>
2025-01-26 11:35:51 +0600

    Update pytelebot.py

--- pytelebot.py
+++ pytelebot.py
@@ -3,8 +3,13 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler,MessageHandler, filters, ContextTypes
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text (
+        'Привет Я Чад как поживаешь?'
+    )
 
+app = Application.builder().token('7973499836:AAHO_7zjhi4LNDXDxLcbW9_ukDVzbQxQJ9s').build()
 
-app = Application.builder().token('ВАШ ТОКЕН').build()
+app.add_handler(CommandHandler('start', start))
 
 app.run_polling()
