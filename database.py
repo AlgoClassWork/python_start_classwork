eb94386b53f619f24badda17b0cbcfda73cd394e
AlgoClassWork <kyxec.play@gmail.com>
2025-01-26 10:12:41 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,40 +1,28 @@
-database = {
-    'Илон Маск' : {
-        'должность': 'уборщик',
-        'эффективность': 100,
-        'портфолио': ['tesla','чистый унитаз']
-    },
-    'Джо Байден' : {
-        'должность': 'спящий дед',
-        'эффективность': 1,
-        'портфолио': ['dota2','белый дом']
-    },
-    'Винни Пух' : {
-        'должность': 'батя',
-        'эффективность': 77,
-        'портфолио': ['медовый завод','аренда шаров']
-    },
-}
+import sqlite3
 
-otvet = input('Чтобы вы хотели узнать о наших сотрудниках?')
-if otvet == 'фамилии':
-    print('Фамилии наших сотрудников:')
-    for surname in database:
-        print('-',surname.split()[1])
-if otvet == 'самый крутой':
-    max_effectivnost = 0
-    samyi_krutoi = ''
+def create_db():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute ( '''
+    CREATE TABLE IF NOT EXISTS ads (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT)  ''' )
+    connection.commit()
+    connection.close()
 
-    for surname in database:
-        if database[surname]['эффективность'] > max_effectivnost:
-            max_effectivnost = database[surname]['эффективность']
+def create_ad(name, description):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute ( '''
+    INSERT INTO ads (name, description) VALUES (?,?) ''',(name, description))
+    connection.commit()
+    connection.close()
 
-    for surname in database:
-        if database[surname]['эффективность'] == max_effectivnost:
-            samyi_krutoi = surname
-
-    print('Самая большая эффективность у', samyi_krutoi)
-    
-if otvet == 'должности':
-    for surname in database:
-        print(surname,' - ',database[surname]['должность'])
+def get_ads():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute ('SELECT * FROM ads')
+    ads = cursor.fetchall()
+    connection.close()
+    return ads
