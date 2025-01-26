762d87de1b4827dbcc151fb1b781d82084c1eda4
AlgoClassWork <kyxec.play@gmail.com>
2025-01-26 15:11:04 +0600

    Update database.py

--- database.py
+++ database.py
@@ -3,64 +3,24 @@ import sqlite3
 def create_db():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
-    cursor.execute ( '''
-    CREATE TABLE IF NOT EXISTS ads (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS names 
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    description TEXT)  ''' )
+    surname TEXT NOT NULL)
+    ''')
     connection.commit()
     connection.close()
 
-def create_ad(name, description):
+
+def add_name(name, surname):
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
-    cursor.execute ( '''
-    INSERT INTO ads (name, description) VALUES (?,?) ''',(name, description))
+    cursor.execute('INSERT INTO names (name, surname) VALUES (?, ?)', (name, surname) )
     connection.commit()
     connection.close()
 
-def get_ads():
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-    cursor.execute ('SELECT * FROM ads')
-    ads = cursor.fetchall()
-    connection.close()
-    return ads
-
-
-
-
-#pip install flask
-from flask import Flask, render_template, request, redirect, url_for
-import data
-
-app = Flask(__name__)
-data.create_db()
-
-@app.route('/')
-def index():
-    ads = data.get_ads()
-    return render_template('index.html', ads=ads)
-
-@app.route('/create', methods=['POST'])
-def create():
-    name = request.form.get('name')
-    description = request.form.get('description')
-    data.create_ad(name, description)
-    return redirect(url_for('index'))
-
-
-app.run()
-
-
-
-<form action="/create" method="post">
-    <input type="text" name="name"><br>
-    <textarea name="description"></textarea><br>
-    <button type="submit">Отправить</button>
-</form>
-
-{% for ad in ads %}
-    <h2>{{ ad[1] }}</h2>
-    <p>{{ ad[2] }}</p>
-{% endfor %}
+create_db()
+add_name('Петя', 'Петров')
+add_name('Вася', 'Пупкин')
+add_name('Мага', 'Чимаев')
