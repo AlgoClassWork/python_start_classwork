1c8966608ec86b77d621b4b04022062b40c03c1b
AlgoClassWork <kyxec.play@gmail.com>
2025-02-03 16:53:43 +0600

    Create ads.py

---
+++ ads.py
@@ -0,0 +1,24 @@
+#data.py
+import sqlite3
+
+def create_db():
+    connection = sqlite3.connect('ads.db')
+    cursor = connection.cursor()
+    cursor.execute ('''
+    CREATE TABLE IF NOT EXISTS ads (
+    name TEXT NOT NULL,
+    description TEXT) ''')
+    connection.commit()
+    connection.close()
+
+def create_ad(name, description):
+    connection = sqlite3.connect('ads.db')
+    cursor = connection.cursor()
+    cursor.execute ('''
+    INSERT INTO ads (name, description) VALUES (?, ?)''', (name, description))
+    connection.commit()
+    connection.close()
+
+create_db()
+#create_ad('Пропала собака', 'Вес 100кг Алабай не кусается')
+#create_ad('Нашел собаку', 'Верну за вознаграждение')
