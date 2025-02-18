8629b9d9bf723c1bf5b97759a12e6b0f9e03d66b
AlgoClassWork <kyxec.play@gmail.com>
2025-02-18 17:43:15 +0600

    Update gorod.py

--- gorod.py
+++ gorod.py
@@ -25,7 +25,7 @@ for i in range(2):
 end_fill()
 
 # Солнышко
-goto(200, 300)
+goto(150, 200)
 color('yellow')
 begin_fill()
 for i in range(18):
@@ -35,7 +35,7 @@ end_fill()
 
 # Основание домика
 penup()
-goto(-250, -50)
+goto(-250, -100)
 pendown()
 color('gray')
 begin_fill()
@@ -48,7 +48,7 @@ end_fill()
 
 # Крыша домика
 penup()
-goto(-250, -50)
+goto(-250, -100)
 pendown()
 color('red')
 begin_fill()
@@ -58,6 +58,97 @@ forward(50)
 left(60)
 forward(150)
 end_fill()
+
+# Окно домика
+penup()
+goto(-180, -150)
+pendown()
+color('white')
+begin_fill()
+for i in range(4):
+    forward(30)
+    right(90)
+end_fill()
+
+# Дверь домика
+penup()
+goto(-100, -200)
+pendown()
+color('brown')
+begin_fill()
+for i in range(2):
+    forward(40)
+    right(90)
+    forward(60)
+    right(90)
+end_fill()
+
+# Дерево 
+penup()
+goto(50, -100)
+pendown()
+color('brown')
+begin_fill()
+for i in range(2):
+    forward(20)
+    right(90)
+    forward(50)
+    right(90)
+end_fill()
+
+penup()
+goto(65, -60)
+pendown()
+color('green')
+begin_fill()
+for i in range(3):
+    forward(50)
+    right(120)
+end_fill()
+
+penup()
+goto(65, -30)
+pendown()
+color('green')
+begin_fill()
+for i in range(3):
+    forward(50)
+    right(120)
+end_fill()
+
+# Дерево 2
+penup()
+goto(150, -150)
+pendown()
+color('brown')
+begin_fill()
+for i in range(2):
+    forward(20)
+    right(90)
+    forward(50)
+    right(90)
+end_fill()
+
+penup()
+goto(165, -110)
+pendown()
+color('green')
+begin_fill()
+for i in range(3):
+    forward(50)
+    right(120)
+end_fill()
+
+penup()
+goto(165, -80)
+pendown()
+color('green')
+begin_fill()
+for i in range(3):
+    forward(50)
+    right(120)
+end_fill()
+
 
 hideturtle()
 exitonclick()
