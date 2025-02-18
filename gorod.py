7d7fc542463e085ff6ee6d38b6b8730daaf778e9
AlgoClassWork <kyxec.play@gmail.com>
2025-02-18 17:07:31 +0600

    Update gorod.py

--- gorod.py
+++ gorod.py
@@ -33,5 +33,20 @@ for i in range(18):
     right(100)
 end_fill()
 
+# Основание домика
+penup()
+goto(-250, -50)
+pendown()
+color('gray')
+begin_fill()
+for i in range(2):
+    forward(200)
+    right(90)
+    forward(100)
+    right(90)
+end_fill()
+
+# Крыша домика
+
 
 exitonclick()
