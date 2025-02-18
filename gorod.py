001856906d3c02e577c139d64ead4d8a306a3501
AlgoClassWork <kyxec.play@gmail.com>
2025-02-18 16:47:08 +0600

    Create gorod.py

---
+++ gorod.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+speed(100)
+
+# земля
+goto(-300, -100)
+color('lightgreen')
+begin_fill()
+forward(600)
+right(90)
+forward(150)
+right(90)
+forward(600)
+right(90)
+forward(150)
+end_fill()
+# небо
+
+
+
+exitonclick()
