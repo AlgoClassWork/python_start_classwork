4e9901db0c4acc74920e2a4aea1cc9fae4b7099f
AlgoClassWork <kyxec.play@gmail.com>
2025-02-18 16:55:07 +0600

    Update gorod.py

--- gorod.py
+++ gorod.py
@@ -9,10 +9,19 @@ begin_fill()
 for i in range(2):
     forward(600)
     right(90)
-    forward(150)
+    forward(200)
     right(90)
 end_fill()
 # небо
+goto(-300, 300)
+color('lightblue')
+begin_fill()
+for i in range(2):
+    forward(600)
+    right(90)
+    forward(400)
+    right(90)
+end_fill()
 
 
 
