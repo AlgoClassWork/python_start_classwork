167eb1c52e37c02fad6d00e47837c500f6561398
AlgoClassWork <kyxec.play@gmail.com>
2025-03-05 17:21:20 +0600

    Update knight_vs_dragon.py

--- knight_vs_dragon.py
+++ knight_vs_dragon.py
@@ -40,5 +40,22 @@ player = Sprite(name='Абу', health=100, damage=10, weapon='меч')
 enemy = Sprite(name='Пикачу', health=150, damage=8, weapon='молнии')
 dragon = Sprite(name='Смауг', health=200, damage=40, weapon='огонь')
 
+print('Бравый герой по имени Абу отправился в путешествие за принцесой и сокровищем')
+print('По дороге к логову дракона он натыкается на ужасное электрическое чудище')
+answer = input('Хотите сразиться с чудищем и стать сильнее?')
+if answer == 'да':
+    player.fight(enemy)
+else:
+    print('Хорошо что вы струсили у него был пульт от ядерки')
+
+if player.health > 0:
+    print('Вы добрались до логова дракона готовтесь к сражению')
+    player.fight(dragon)
+
+if player.health > 0:
+    print('Вы забрали много сокровище и принцессу живвите счастливо')
+else:
+    print('Вы слишком слабый для этого мира')
+
 
 
