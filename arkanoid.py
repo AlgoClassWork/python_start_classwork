f4b2676711b001af886efd7e1229f73f6a7ade92
AlgoClassWork <kyxec.play@gmail.com>
2025-03-09 17:39:47 +0600

    Update arkanoid.py

--- arkanoid.py
+++ arkanoid.py
@@ -1,8 +1,9 @@
 from pygame import *
 
 # Общее описание персонажей
-class GameSprite:
+class GameSprite(sprite.Sprite):
     def __init__(self, img, x, y):
+        super().__init__()
         self.image = image.load(img)
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -15,6 +16,13 @@ class GameSprite:
 # Создание обьектов
 platform = GameSprite(img='platform.png',x=300,y=450)
 ball = GameSprite(img='ball.png', x=300, y=300)
+
+enemys = sprite.Group()
+x = 0
+for i in range(10):
+    enemy = GameSprite(img='enemy.png',x=x,y=0) 
+    x += 70
+    enemys.add(enemy)
 
 # создание экрана
 window = display.set_mode( (700,500) )
@@ -32,6 +40,7 @@ while game:
     # отображение персонажей
     platform.show()
     ball.show()
+    enemys.draw(window)
     # обновление кадров
     display.update()
     # настройка частоты кадров
