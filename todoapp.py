d2ac8b082c1c6578ed1c416e5ab9a6f82ccb5fc0
AlgoClassWork <kyxec.play@gmail.com>
2025-03-09 15:22:34 +0600

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -8,9 +8,16 @@ class ToDoApp(App):
         input_layout = BoxLayout(size_hint=(0.95, 0.1), pos_hint={'center_x': 0.5, 'top': 0.95 })
         self.text_input = TextInput(font_size=30)
         add_button = Button(text='Добавить', size_hint=(0.15, 0.95), background_color=(0.3,1,0.3,1))
+        add_button.on_press = self.add_task
         input_layout.add_widget(self.text_input)
         input_layout.add_widget(add_button)
         return input_layout
+    
+    def add_task(self):
+        task_text = self.text_input.text.strip()
+        if task_text:
+            task = {'описание': task_text, 'статус': False}
+            #task_widgget = TaskWidget(task)
 
 app = ToDoApp()
 app.run()
