d2b7ed001db690e0a43491a01ad69518dfbc603c
AlgoClassWork <kyxec.play@gmail.com>
2025-03-09 14:59:53 +0600

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -1,7 +1,16 @@
 from kivy.app import App
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 class ToDoApp(App):
-    pass
+    def build(self):
+        input_layout = BoxLayout()
+        self.text_input = TextInput(font_size=30)
+        add_button = Button(text='Добавить')
+        input_layout.add_widget(self.text_input)
+        input_layout.add_widget(add_button)
+        return input_layout
 
 app = ToDoApp()
 app.run()
