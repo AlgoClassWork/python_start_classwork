b8ccc50a11e5f4973ec4bd6680519a4ccfc2f755
AlgoClassWork <kyxec.play@gmail.com>
2025-03-12 15:41:44 +0600

    Create kikivy.py

---
+++ kikivy.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+class MyButton(Button):
+    def __init__(self, screen, direction, goal, **data):
+        super().__init__(**data)
+        self.screen = screen
+        self.direction = direction
+        self.goal = goal
+
+    def on_press(self):
+        self.screen.manager.transition.direction = self.direction
+        self.screen.manager.current = self.goal
+
+class MainScreen(Screen):
+    def __init__(self, **data):
+        super().__init__(**data)
+        main_layout = BoxLayout()
+        button_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        text = Label(text='Выбери экран')
+        button_layout.add_widget( MyButton(self, direction='down', goal='first', text='1') )
+        button_layout.add_widget( MyButton(self, direction='left', goal='second', text='2') )
+        button_layout.add_widget( MyButton(self, direction='up', goal='third', text='3') )
+        button_layout.add_widget( MyButton(self, direction='right', goal='fourth', text='4') )
+        main_layout.add_widget(text)
+        main_layout.add_widget(button_layout)
+        self.add_widget(main_layout)
+
+class MyApp(App):
+    def build(self):
+        screen_manager = ScreenManager()
+        screen_manager.add_widget( MainScreen(name='main') )
+        return screen_manager
+
+app = MyApp()
+app.run()
