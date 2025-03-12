ff83b10353ef076c06cddd5ebc53c34b15b83c27
AlgoClassWork <kyxec.play@gmail.com>
2025-03-12 16:18:24 +0600

    Update kikivy.py

--- kikivy.py
+++ kikivy.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 class MyButton(Button):
@@ -39,12 +40,32 @@ class FirstScreen(Screen):
         main_layout.add_widget(button_back)
         self.add_widget(main_layout)
 
+class SecondScreen(Screen):
+    def __init__(self, **data):
+        super().__init__(**data)
+        main_layout = BoxLayout(orientation='vertical')
+        h1 = BoxLayout(size_hint=(0.7, None), height='50px', pos_hint={'center_x':0.5})
+        h2 = BoxLayout(size_hint=(0.7, None), height='100px', pos_hint={'center_x':0.5})
+        text = Label(text='Выбор')
+        text_input = Label(text='Введите пароль')
+        password_input = TextInput()
+        button = Button(text='OK')
+        button_back = MyButton(self, direction='right', goal='main', text='назад')
+        h1.add_widget(text_input)
+        h1.add_widget(password_input)
+        h2.add_widget(button)
+        h2.add_widget(button_back)
+        main_layout.add_widget(text)
+        main_layout.add_widget(h1)
+        main_layout.add_widget(h2)
+        self.add_widget(main_layout)
 
 class MyApp(App):
     def build(self):
         screen_manager = ScreenManager()
         screen_manager.add_widget( MainScreen(name='main') )
         screen_manager.add_widget( FirstScreen(name='first') )
+        screen_manager.add_widget( SecondScreen(name='second') )
         return screen_manager
 
 app = MyApp()
