a5c40b259dcc4e1008b67a85b77ad3c673be17f0
AlgoClassWork <kyxec.play@gmail.com>
2025-03-15 10:41:36 +0600

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -10,6 +10,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image 
 
+from kivy.animation import Animation
 from kivy.properties import ObjectProperty
 from kivy.utils import get_color_from_hex
 from kivy.core.window import Window
@@ -35,6 +36,7 @@ class TaskWidget(BoxLayout):
         checkbox.bind(active=self.on_checkbox_active)
         self.label = Label(text=self.task['description'], markup=True, font_size=18, color=get_color_from_hex('#FFFFFF'))
         delete_button = ImageButton(source='delete_icon.png', size_hint=(None, None), size=(30,30))
+        delete_button.bind(on_press=self.delete_task)
 
         self.add_widget(checkbox)
         self.add_widget(self.label)
@@ -50,8 +52,14 @@ class TaskWidget(BoxLayout):
         else:
             self.label.text = f"{self.task['description']}"
 
-    def delete_task(self):
-        pass
+    def delete_task(self, instance):
+        if self.task in self.tasks_list:
+            self.tasks_list.remove(self.task)
+
+        instance.disabled = True
+        anim = Animation(opacity=0, duration=0.3)
+        anim.bind(on_complete=lambda *args: self.parent.remove_widget(self))
+        anim.start(self)
 
 class TodoApp(App):
     def build(self):
@@ -83,7 +91,9 @@ class TodoApp(App):
             self.tasks.append(task)
             task_widget = TaskWidget(task=task, tasks_list=self.tasks)
             self.tasks_layout.add_widget(task_widget)
+            self.text_input.text = ''
+            task_widget.opacity = 0
+            Animation(opacity=1, duration=0.3).start(task_widget)
             
-
 app = TodoApp()
 app.run()
