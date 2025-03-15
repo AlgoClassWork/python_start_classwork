c16acd6e47e424439180bddf3920be23aa22a9e0
AlgoClassWork <kyxec.play@gmail.com>
2025-03-15 14:25:00 +0600

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -5,6 +5,8 @@ from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.label import Label
 
+from kivy.properties import ObjectProperty
+
 class TaskWidget(BoxLayout):
     task = ObjectProperty(None)
     tasks_list = ObjectProperty(None)
@@ -17,7 +19,6 @@ class TaskWidget(BoxLayout):
 
         self.add_widget(checkbox)
         self.add_widget(self.label)
-        self.add_widget(delete_button)
 
 
 class TodoApp(App):
@@ -26,14 +27,14 @@ class TodoApp(App):
 
         root = BoxLayout()
 
-        input_layout = BoxLayout(orientation='horizontal', size_hint=(0.9, None), height=50, pos_hint={'center_x': 0.5, 'top': 0.95})
+        self.input_layout = BoxLayout(orientation='horizontal', size_hint=(0.9, None), height=50, pos_hint={'center_x': 0.5, 'top': 0.95})
         self.text_input = TextInput(multiline=False, font_size=18)
         add_button = Button(text='Add', size_hint=(None, None), size=(100, 50))
         add_button.bind(on_press=self.add_task)
-        input_layout.add_widget(self.text_input)
-        input_layout.add_widget(add_button)
+        self.input_layout.add_widget(self.text_input)
+        self.input_layout.add_widget(add_button)
 
-        root.add_widget(input_layout)
+        root.add_widget(self.input_layout)
 
         return root
 
