d9c1d3fac3f68c0f5831e568a38bf26612fab8e0
AlgoClassWork <kyxec.play@gmail.com>
2025-03-16 15:45:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 class MyButton(Button):
     def __init__(self, screen, goal, **data):
@@ -44,7 +44,10 @@ class FirstScreen(Screen):
 
 class MyApp(App):
     def build(self):
-        return FirstScreen()
+        screen_manager = ScreenManager()
+        screen_manager.add_widget( MainScreen(name='main') )
+        screen_manager.add_widget( FirstScreen(name='first') )
+        return screen_manager
     
 app = MyApp()
 app.run()
