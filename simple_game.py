f5dca2656d6645c051aedf257d25572c725a5ade
AlgoClassWork <kyxec.play@gmail.com>
2025-03-17 17:43:17 +0600

    Update simple_game.py

--- simple_game.py
+++ simple_game.py
@@ -1,50 +1,16 @@
+#pip install pygame
 from pygame import *
 
-window = display.set_mode( (700,500) )
-display.set_caption('Догонялки')
-
-background = image.load('фон.png')
-background = transform.scale(background, (700,500) )
-
-pica = transform.scale(image.load('pica.png'), (100,100) )
-gita = transform.scale(image.load('gita.png'), (100,100) )
-
-pica_x, pica_y = 200, 200
-gita_x, gita_y = 400, 200
-
-clock = time.Clock()
+window = display.set_mode( (700, 500) )
+back = image.load('back.png')
+back = transform.scale( back, (700, 500) )
 
 game = True
 while game:
-
     for some_event in event.get():
         if some_event.type == QUIT:
             game = False
 
-    window.blit(background, (0,0) )
-    window.blit(pica, (pica_x, pica_y))
-    window.blit(gita, (gita_x, gita_y))
-
-    keys = key.get_pressed()
-
-    if keys[K_w] and pica_y > 0:
-        pica_y -= 3
-    if keys[K_s] and pica_y < 400:
-        pica_y += 3
-    if keys[K_a] and pica_x > 0:
-        pica_x -= 3
-    if keys[K_d] and pica_x < 600:
-        pica_x += 3
-
-    if pica_x > gita_x:
-        gita_x += 1
-    if pica_x < gita_x:
-        gita_x -= 1
-    if pica_y > gita_y:
-        gita_y += 1
-    if pica_y < gita_y:
-        gita_y -= 1
-
-    
-    clock.tick(60)
+    window.blit(back, (0, 0))
     display.update()
+    
