58283fe0d5917f5a30a4388bf949cea1861b3f87
AlgoClassWork <kyxec.play@gmail.com>
2025-03-29 11:05:04 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,95 +1,42 @@
-from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.checkbox import CheckBox
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QPushButton,
+    QVBoxLayout, QGroupBox, QRadioButton, QHBoxLayout)
+# Создание приложения
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Memory card')
+window.show()
+# Создание элементов интерфейса
+question = QLabel('Как переводится apple?')
+button = QPushButton('ответить')
+# Создание формы с вариантами ответов
+answer_box = QGroupBox('Варианты ответов:')
+button1 = QRadioButton('груша')
+button2 = QRadioButton('яблоко')
+button3 = QRadioButton('апельсин')
+button4 = QRadioButton('банан')
 
-from kivy.core.window import Window
-from kivy.animation import Animation
-from kivy.properties import ObjectProperty
-
-Window.clearcolor = (1,1,1,1)
-
-class TaskWidget(BoxLayout):
-    task = ObjectProperty(None)
-    tasks = ObjectProperty(None)
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        self.size_hint_y = None
-        self.height = 50
-
-        checkbox = CheckBox(size_hint=(0.1, 1))
-        self.label = Label(text=self.task['описание'], color=(0,0,0,1), font_size=30)
-        delete_button = Button(text='Удалить', size_hint=(0.33, 1),
-        background_color=(1, 0.5, 0.5, 1), font_size=30)
-
-        self.add_widget(checkbox)
-        self.add_widget(self.label)
-        self.add_widget(delete_button)
-
-        checkbox.bind(active=self.checkbox_activate)
-        delete_button.bind(on_press=self.delete_task)
-
-    def delete_task(self, instance):
-        if self.task in self.tasks:
-            self.tasks.remove(self.task)
-
-        instance.disabled = True
-        animation = Animation(opacity=0, duration=1)
-        animation.bind(on_complete=lambda *data: self.parent.remove_widget(self))
-        animation.start(self)
-
-    def checkbox_activate(self, instance, value):
-        self.task['статус'] = value
-        self.update_label()
-
-    def update_label(self):
-        if self.task['статус']:
-            self.label.text = f'Выполнено: {self.task['описание']} '
-        else:
-            self.label.text = f' {self.task['описание']} '
-
-
-class ToDoApp(App):
-    def build(self):
-        self.tasks = []
-
-        main_layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
-
-        self.text_input = TextInput(font_size=30)
-        add_button = Button(text='Добавить', size_hint=(0.3, 1),
-        font_size=30, background_color=(0.7, 1, 0.7, 1) )
-        add_button.on_press = self.add_task
-        self.input_layout = BoxLayout(size_hint=(1, 0.1),
-        pos_hint={'center_y':0.95}, spacing='10px')
-
-        self.tasks_layout = BoxLayout(orientation='vertical', spacing=10, size_hint_y=None)
-        self.tasks_layout.bind(minimum_height=self.tasks_layout.setter('height'))
-        scroll_view = ScrollView(pos_hint={'center_y':0.4})
-        scroll_view.add_widget(self.tasks_layout)
-        
-        self.input_layout.add_widget(self.text_input)
-        self.input_layout.add_widget(add_button)
-
-        main_layout.add_widget(self.input_layout)
-        main_layout.add_widget(scroll_view)
-
-        return main_layout
-    
-    def add_task(self):
-        task_text = self.text_input.text.strip()
-        if task_text:
-            task = {'описание': task_text, 'статус': False}
-            self.tasks.append(task)
-            task_widget = TaskWidget(task = task, tasks = self.tasks)
-            self.tasks_layout.add_widget(task_widget)
-            self.text_input.text = ''
-            task_widget.opacity = 0
-            Animation(opacity=1, duration=1).start(task_widget)
-    
-app = ToDoApp()
-app.run()
-
+answer_line = QVBoxLayout()
+h1 = QHBoxLayout()
+h2 = QHBoxLayout()
+h1.addWidget(button1)
+h1.addWidget(button2)
+h2.addWidget(button3)
+h2.addWidget(button4)
+answer_line.addLayout(h1)
+answer_line.addLayout(h2)
+answer_box.setLayout(answer_line)
+# Размещение элементов интерфейса
+main_line = QVBoxLayout()
+main_line.addWidget(question, alignment=(Qt.AlignHCenter | Qt.AlignTop) )
+main_line.addWidget(answer_box)
+main_line.addWidget(button)
+window.setLayout(main_line)
+# Стилизация элементов интерфейса
+window.setStyleSheet('background:white')
+question.setStyleSheet('font-size:40px;')
+button.setStyleSheet('font-size:30px; padding:20px; background-color:lightgreen; border: 5px solid black')
+answer_box.setStyleSheet('font-size:30px; padding:30px; background-color:lightgray')
+# Запуск приложения
+app.exec()
