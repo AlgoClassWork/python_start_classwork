b8e29a118d19b952dc86619b08b271eed3bbbaca
AlgoClassWork <kyxec.play@gmail.com>
2025-04-14 15:48:51 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,29 +1,19 @@
-import pandas 
-import matplotlib.pyplot
+import pandas
 
 data = pandas.read_csv('countries.csv')
 
-# ПОДГОТОВКА ДАННЫХ
 def repair(value):
     value = str(value)
     return float(value.replace(',','.'))
 
-columns = ['Pop','Coastline','Migration','Mortality','Literacy',
-           'Phones','Arable','Crops','Other','Climate','Birthrate',
-           'Deathrate','Agriculture','Industry','Service','GDP']
+columns = ['Density','Coastline','Migration','Mortality','GDP',
+           'Literacy','Phones','Arable','Crops','Other','Climate',
+           'Birthrate','Deathrate','Agriculture','Industry','Service']
 
 for column in columns:
-    data[column] = data[column].apply(repair)
+    data[column] = data[column].apply( repair )
 
 for column in columns:
-    data[column].fillna(data[column].mean(), inplace=True)
+    data[column] = data[column].fillna( data[column].mean() )
 
-# АНАЛИЗ И ВИЗУАЛИЗАЦИЯ ДАННЫХ
-# 1. Влияет ли уровень ВВП на грамотность населения
-#data.plot(x='GDP', y='Literacy', kind='scatter')
-# 2. Проверка числа стран по регионам
-#data['Region'].value_counts().plot(kind='pie')
-# 3. Статистика смертности по регионам
-data.groupby('Region')['Deathrate'].mean().plot(kind='barh')
-
-matplotlib.pyplot.show()
+print(data.info())
