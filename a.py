fd4982621696cd7de40f9d93b1c19d434800db01
AlgoClassWork <kyxec.play@gmail.com>
2025-04-14 17:37:45 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,49 +1,61 @@
-import pandas
-import matplotlib.pyplot
+from telegram import Update
+from telegram.ext import (ApplicationBuilder, CommandHandler,
+                          MessageHandler, ContextTypes, filters)
 
-data = pandas.read_csv('countries.csv')
+board = [ '⬜','⬜','⬜','⬜','⬜','⬜','⬜','⬜','⬜' ] 
 
-# Подготовка данных
-def repair(value):
-    value = str(value)
-    return float(value.replace(',','.'))
+def check_winner():
+    win_combo = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], #Горизонтали
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], #Вертикали
+        [0, 4, 8], [2, 4, 6], #Диагонали
+    ]
+    for combo in win_combo:
+        a, b, c = combo 
+        if board[a] == board[b] == board[c] and board[a] != '⬜':
+            return board[a]
+    return None
 
-columns = ['Density','Coastline','Migration','Mortality','GDP',
-           'Literacy','Phones','Arable','Crops','Other','Climate',
-           'Birthrate','Deathrate','Agriculture','Industry','Service']
+def check_draw():
+    return all(cell != '⬜' for cell in board)
+        
+def format_bord():
+    return f'{board[0]}   {board[1]}   {board[2]}\n' \
+           f'\n' \
+           f'{board[3]}   {board[4]}   {board[5]}\n' \
+           f'\n' \
+           f'{board[6]}   {board[7]}   {board[8]}\n' 
 
-for column in columns:
-    data[column] = data[column].apply( repair )
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_player, board
+    current_player = '❌'
+    board = [ '⬜','⬜','⬜','⬜','⬜','⬜','⬜','⬜','⬜' ] 
+    await update.message.reply_text('Игра началась! Ходит ❌. Пиши номер клетки (0-8):')
+    await update.message.reply_text( format_bord() )
 
-for column in columns:
-    data[column] = data[column].fillna( data[column].mean() )
+async def move(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_player
+    try:
+        pos = int(update.message.text) 
 
-# АНАЛИЗ ДАННЫХ
-# Гипотеза 1 ВВП зависит от уровня Грамотности населения
-# Сложный способ 
-def do_wealth(gdp):
-    if gdp < 15000:
-        return 'poor'
-    elif gdp >= 15000 and gdp <= 30000:
-        return 'normal'
-    else:
-        return 'rich'
+        if 0 <= pos <= 8 and board[pos] == '⬜':
+            board[pos] = current_player
+            await update.message.reply_text( format_bord() )
 
-data['Wealth'] = data['GDP'].apply(do_wealth)
-print(data.groupby('Wealth')['Literacy'].mean())
-# Простой способ
-#data.plot(x='GDP', y='Literacy', kind='scatter')
+            winner = check_winner()
+            if winner:
+                await update.message.reply_text(f'{winner} победил! Напиши /start для новой игры')
 
-# Гипотеза 2 Выход к морю влияет на показатель ВВП
-def sea(coasline):
-    if coasline > 0:
-        return 'open'
-    else:
-        return 'close'
+            if check_draw():
+                await update.message.reply_text('Ничья напиши /start для новой игры')
 
-data['Sea'] = data['Coastline'].apply(sea)
-print(data.groupby('Sea')['GDP'].mean())
+            current_player = '⭕' if current_player == '❌' else '❌'
+        else:
+            await update.message.reply_text('Клетка занята или вы написали неккоректное число')
+    except:
+        await update.message.reply_text('Пиши числа от 0 до 8')
 
-data.groupby('Sea')['GDP'].mean().plot(kind='bar')
-
-matplotlib.pyplot.show()
+app = ApplicationBuilder().token('7137462050:AAHh2d2TVPe3xS88xQ1vkZ23jax9usVgqIQ').build()
+app.add_handler(CommandHandler('start', start))
+app.add_handler(MessageHandler(filters.TEXT, move))
+app.run_polling()
