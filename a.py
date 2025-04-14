b26c6d4f2e6cbda1845548e792e34ead9818e782
AlgoClassWork <kyxec.play@gmail.com>
2025-04-14 16:18:18 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,9 @@
 import pandas
+import matplotlib.pyplot
 
 data = pandas.read_csv('countries.csv')
 
+# Подготовка данных
 def repair(value):
     value = str(value)
     return float(value.replace(',','.'))
@@ -16,4 +18,20 @@ for column in columns:
 for column in columns:
     data[column] = data[column].fillna( data[column].mean() )
 
-print(data.info())
+# АНАЛИЗ ДАННЫХ
+# Гипотеза 1 ВВП зависит от уровня Грамотности населения
+# Сложный способ 
+def do_wealth(gdp):
+    if gdp < 15000:
+        return 'poor'
+    elif gdp >= 15000 and gdp <= 30000:
+        return 'normal'
+    else:
+        return 'rich'
+
+data['Wealth'] = data['GDP'].apply(do_wealth)
+print(data.groupby('Wealth')['Literacy'].mean())
+# Простой способ
+data.plot(x='GDP', y='Literacy', kind='scatter')
+
+matplotlib.pyplot.show()
