97d584bb64c8c6e39ae27d16a26f083050908068
AlgoClassWork <kyxec.play@gmail.com>
2025-04-27 10:34:08 +0600

    Update kaldi_model.py

--- kaldi_model.py
+++ kaldi_model.py
@@ -10,22 +10,16 @@ import json
 def initial_convert(filename):
    audio = AudioSegment.from_file(filename)
 
-
    audio = audio.set_channels(1)  # Преобразуем в моно
    audio = audio.set_frame_rate(16000)  # Устанавливаем частоту дискретизации на 16000 Гц
    audio = audio.set_sample_width(2)  # Устанавливаем ширину выборки (16 бит)
 
-
    audio.export(f'{filename.split(".")[0]}_converted.wav', format='wav')
-
-   # Проверяем параметры сохранённого файла
-   # audio = AudioSegment.from_file(output_file)
-   # print(f"Каналы: {audio.channels}, Частота: {audio.frame_rate}, Ширина выборки: {audio.sample_width * 8} бит")
 
 model = Model('vosk-model-small-ru-0.22')
 rec = KaldiRecognizer(model, 16000)
-initial_convert("input_good.wav")
-wf = wave.open("input_good_converted.wav", "rb")
+initial_convert("first_test.wav")
+wf = wave.open("first_test_converted.wav", "rb")
 
 
 full_text = []
