bed03d7473cef6367d6f8f615bb0ceec743ab1d7
AlgoClassWork <kyxec.play@gmail.com>
2025-05-04 15:50:34 +0600

    Update image_class.py

--- image_class.py
+++ image_class.py
@@ -1,1 +1,20 @@
+# версия python ниже 3.13
+# скачать visual c++
+# pip install pillow
+from PIL import Image
+import tensorflow_hub
+import tensorflow
+import requests
+import numpy
+
+model = tensorflow_hub.load('https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_s/classification/2')
 labels = requests.get("https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt").text.splitlines()
+
+image = Image.open('bars.jpg').resize((200,200))
+image = numpy.expand_dims(numpy.array(image) / 255, axis=0 ).astype(numpy.float32)
+
+predict = model(image)
+probs = tensorflow.nn.softmax(predict)[0].numpy()
+
+for predict in probs.argsort()[-5:] :
+    print(f' {labels[predict]} : {probs[predict] * 100 } %')
