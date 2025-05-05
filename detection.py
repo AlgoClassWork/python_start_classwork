70d3dc9b20ca866779491b0dcdc98407f7693bba
AlgoClassWork <kyxec.play@gmail.com>
2025-05-05 16:31:13 +0600

    Update detection.py

--- detection.py
+++ detection.py
@@ -13,3 +13,31 @@ for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 # Отображение изображения
 plt.imshow(image), plt.axis('off'), plt.show()
+
+
+
+# для видео
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('face.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('Камера', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
