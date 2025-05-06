7b8a886f65a0a3c9fc3890b20c47a7ee9e41e3b4
AlgoClassWork <kyxec.play@gmail.com>
2025-05-06 10:29:31 +0600

    Update detection.py

--- detection.py
+++ detection.py
@@ -14,6 +14,29 @@ result = face.detectMultiScale(gray)
 for (x, y, w, h) in result:
     cv2.rectangle(rgb, (x, y), (x + w, y + h), (0,255,0), 1)
 
+
+
+# ВИДЕО ДЕТЕКЦИЯ
+import cv2
+
+face = cv2.CascadeClassifier('face.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    result = face.detectMultiScale(gray)
+
+    for (x, y, w, h) in result:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 1)
+
+    cv2.imshow('Webcam', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 plt.imshow(rgb)
 plt.axis('off')
 plt.show()
