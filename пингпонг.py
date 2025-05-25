cdc0e45da4f6527bb30e92dcc38384cfc2d629ec
AlgoClassWork <kyxec.play@gmail.com>
2025-05-25 12:20:01 +0600

    Update пингпонг.py

--- пингпонг.py
+++ пингпонг.py
@@ -20,6 +20,7 @@ class GameSprite(sprite.Sprite):
 player = GameSprite('block.png', 20, 100, 10, 200, 0)
 computer = GameSprite('block.png', 20, 100, 670, 200, 3)
 ball = GameSprite('ball.png', 50, 50, 350, 200, 5)
+speed_x, speed_y = ball.speed, ball.speed
 
 # Настройка окна игры
 window = display.set_mode((700, 500))  # Установка размеров окна
@@ -40,6 +41,13 @@ while True:
     player.show()
     computer.show()
     ball.show()
+
+    mouse_x, mouse_y = mouse.get_pos()
+    player.rect.centery = mouse_y
+
+    ball.rect.x += speed_x
+    if ball.rect.x > 650 or ball.rect.x < 0:
+        speed_x *= -1
 
     # Обновление экрана
     display.update()
