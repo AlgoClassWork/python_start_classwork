ffd54dc7a56d8a95f993c9e3c1208cf53504b96c
AlgoClassWork <kyxec.play@gmail.com>
2025-07-25 10:41:41 +0600

    Update simplee_game.py

--- simplee_game.py
+++ simplee_game.py
@@ -7,14 +7,19 @@ background = transform.scale(background, (700, 500) )
 # Загрузка картинки для персонажа
 pica = image.load('pica.png')
 pica = transform.scale(pica, (150, 150) )
+pica_x, pica_y = 200, 200
 # Игровой цикл
 while True:
     # Обработка нажатия на крестик
     for some_event in event.get():
         if some_event.type == QUIT:
             exit()
+
+    keys = key.get_pressed()
+    if keys[K_w] and pica_y > 0:
+        pica_y -= 1
     # Отображение картинок
     window.blit(background, (0, 0) )
-    window.blit(pica, (200, 200) )
+    window.blit(pica, (pica_x, pica_y) )
     # Обновление кадров игры
     display.update()
