fb4d23114ecad54904b76ecbb5ae6bf023f40c94
AlgoClassWork <kyxec.play@gmail.com>
2025-07-25 10:49:58 +0600

    Update simplee_game.py

--- simplee_game.py
+++ simplee_game.py
@@ -16,8 +16,14 @@ while True:
             exit()
 
     keys = key.get_pressed()
-    if keys[K_w] and pica_y > 0:
+    if keys[K_w] and pica_y > -50:
         pica_y -= 1
+    if keys[K_s] and pica_y < 400:
+        pica_y += 1
+    if keys[K_a] and pica_x > -50:
+        pica_x -= 1
+    if keys[K_d] and pica_x < 600:
+        pica_x += 1
     # Отображение картинок
     window.blit(background, (0, 0) )
     window.blit(pica, (pica_x, pica_y) )
