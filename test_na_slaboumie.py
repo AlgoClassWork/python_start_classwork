1f41fd11a85726bc67ad3eb0a53f0d5dfa80072c
AlgoClassWork <kyxec.play@gmail.com>
2025-10-11 13:41:10 +0600

    Create test_na_slaboumie.py

---
+++ test_na_slaboumie.py
@@ -0,0 +1,48 @@
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QVBoxLayout, QHBoxLayout,
+    QLabel, QGroupBox, 
+    QRadioButton, QPushButton
+)
+# Создание обьектов интерфейса
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Тест на слабоумие')
+
+label_question = QLabel('Как тебя зовут?')
+button_ok = QPushButton('Ответить')
+# Форма с вариантами ответов
+group_answer = QGroupBox('Варианты ответов')
+rbutton1 = QRadioButton('Забыл')
+rbutton2 = QRadioButton('Зеленый')
+rbutton3 = QRadioButton('Сорок семь')
+rbutton4 = QRadioButton('Тут нет моего имени')
+
+
+group_result = QGroupBox('Результаты')
+# Размещение обьектов интерфейса
+line_main = QVBoxLayout()
+
+# Размещение для формы с вариантами ответов
+line_answer_main = QVBoxLayout()
+line_answer_h1 = QHBoxLayout()
+line_answer_h2 = QHBoxLayout()
+line_answer_main.addLayout(line_answer_h1)
+line_answer_main.addLayout(line_answer_h2)
+line_answer_h1.addWidget(rbutton1)
+line_answer_h1.addWidget(rbutton2)
+line_answer_h2.addWidget(rbutton3)
+line_answer_h2.addWidget(rbutton4)
+group_answer.setLayout(line_answer_main)
+
+
+line_main.addWidget(label_question)
+line_main.addWidget(group_answer)
+line_main.addWidget(group_result)
+line_main.addWidget(button_ok)
+
+window.setLayout(line_main)
+# Запуск приложения
+group_result.hide()
+window.show()
+app.exec()
