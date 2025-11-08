a7458cf2b3dec9f872565a80464d341c080ec317
AlgoClassWork <kyxec.play@gmail.com>
2025-11-08 12:42:17 +0600

    Create easy.py

---
+++ easy.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import (
+   QApplication, QWidget,
+   QFileDialog, # Диалог открытия файлов (и папок)
+   QLabel, QPushButton, QListWidget,
+   QHBoxLayout, QVBoxLayout
+)
+
+app = QApplication([])
+window = QWidget()       
+window.resize(700, 500) 
+window.setWindowTitle('Easy Editor')
+image_lable = QLabel("Картинка")
+folder_button = QPushButton("Папка")
+files_list = QListWidget()
+
+
+left_button = QPushButton("Лево")
+right_button = QPushButton("Право")
+flip_button = QPushButton("Зеркало")
+sharp_button = QPushButton("Резкость")
+gray_button = QPushButton("Ч/Б")
+
+
+main_line = QHBoxLayout()       
+col1 = QVBoxLayout()         
+col2 = QVBoxLayout()
+col1.addWidget(folder_button)     
+col1.addWidget(files_list)     
+col2.addWidget(image_lable) 
+row_buttons = QHBoxLayout()  
+row_buttons.addWidget(left_button)
+row_buttons.addWidget(right_button)
+row_buttons.addWidget(flip_button)
+row_buttons.addWidget(sharp_button)
+row_buttons.addWidget(gray_button)
+col2.addLayout(row_buttons)
+
+
+main_line.addLayout(col1, 20)
+main_line.addLayout(col2, 80)
+window.setLayout(main_line)
+
+
+window.show()
+app.exec()
