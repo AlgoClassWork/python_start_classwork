c480494da745c5e9c339652c1f4461c16158ee38
AlgoClassWork <kyxec.play@gmail.com>
2025-12-06 13:57:03 +0600

    Create maze-labirint.py

---
+++ maze-labirint.py
@@ -0,0 +1,45 @@
+from pygame import *
+
+class GameSprite():
+    def __init__(self, img, cord_x, cord_y, width, height, speed):
+        self.image = transform.scale(image.load(img), (width, height))
+        self.speed = speed
+        self.x = cord_x
+        self.y = cord_y
+        self.width = width
+        self.height = height
+
+    def show(self):
+        window.blit( self.image, (self.x, self.y) )
+
+class Player(GameSprite):
+    def move(self):
+        keys = key.get_pressed() 
+        if keys[K_d] and self.x < 700 - self.width:
+            self.x += self.speed
+        if keys[K_a] and self.x > 0:
+            self.x -= self.speed
+        if keys[K_w] and self.y > 0:
+            self.y -= self.speed
+        if keys[K_s] and self.y < 500 - self.height:
+            self.y += self.speed
+
+window = display.set_mode( size=(700, 500) )
+display.set_caption('Лабиринт')
+
+back = transform.scale(image.load('back.jpg'), (700,500))
+
+player = Player('player.png', 100, 100, 50, 50, 5)
+enemy = GameSprite('enemy.png', 400, 100, 50, 50, 5)
+
+while True:
+    for some_event in event.get():
+        if some_event.type == QUIT:
+            exit()
+
+    window.blit(back, (0, 0))
+    player.show()
+    enemy.show()
+    player.move()
+
+    display.update()
