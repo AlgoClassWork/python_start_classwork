61b86405c851e975d3ec5a3fce6f21db40e893e6
AlgoClassWork <kyxec.play@gmail.com>
2025-12-07 10:32:52 +0300

    Update maze-labirint.py

--- maze-labirint.py
+++ maze-labirint.py
@@ -1,5 +1,6 @@
 from pygame import *
 
+# Класс спрайта
 class GameSprite(sprite.Sprite):
     def __init__(self, img, cord_x, cord_y, width, height, speed):
         self.image = transform.scale(image.load(img), (width, height))
@@ -10,10 +11,13 @@ class GameSprite(sprite.Sprite):
         self.width = width
         self.height = height
 
+    # Отображение спрайта
     def show(self):
         window.blit( self.image, (self.rect.x, self.rect.y) )
-    
+
+# Класс игрока 
 class Player(GameSprite):
+    # Движение игрока
     def move(self):
         keys = key.get_pressed() 
         if keys[K_d] and self.rect.x < 700 - self.width:
@@ -25,6 +29,7 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < 500 - self.height:
             self.rect.y += self.speed
 
+# Класс стены
 class Wall(sprite.Sprite):
     def __init__(self, cord_x, cord_y, width, height):
         self.width = width
@@ -34,7 +39,8 @@ class Wall(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = cord_x
         self.rect.y = cord_y
-
+        
+    # Отображение стены
     def show(self):
         window.blit( self.image, (self.rect.x, self.rect.y) )
 
@@ -44,31 +50,43 @@ display.set_caption('Лабиринт')
 # Загрузка фона
 back = transform.scale(image.load('back.jpg'), (700,500))
 # Создание спрайтов
-player = Player('player.png', 100, 100, 50, 50, 10)
-enemy = GameSprite('enemy.png', 400, 100, 50, 50, 5)
+player = Player('player.png', 0, 450, 50, 50, 10)
+enemy = GameSprite('enemy.png', 650, 450, 50, 50, 5)
+
 wall1 = Wall(100, 200, 400, 20)
 wall2 = Wall(100, 200, 20, 300)
 wall3 = Wall(500, 100, 20, 300)
 
+walls = [wall1, wall2, wall3]
+
+# Основной цикл игры
 clock = time.Clock()
 while True:
+    # Обработка событий
     for some_event in event.get():
         if some_event.type == QUIT:
             exit()
-
+    # Отрисовка фона
     window.blit(back, (0, 0))
     # Отображение спрайтов
     player.show()
     enemy.show()
-    wall1.show()
-    wall2.show()
-    wall3.show()
+    for wall in walls:
+        wall.show()
     # Движение спрайтов
     player.move()
-    
+
+    # Проверка столкновений с врагом
     if sprite.collide_rect(player, enemy):
         player.rect.x = 100
         player.rect.y = 100
 
+    # Проверка столкновений с стенами
+    for wall in walls:
+        if sprite.collide_rect(player, wall):
+            player.rect.x = 0
+            player.rect.y = 450
+
+    # Обновление экрана
     display.update()
     clock.tick(60)
