02a2feb092e922bf1c0d35f8323543c25ff5d1e1
AlgoClassWork <kyxec.play@gmail.com>
2025-12-07 10:02:28 +0300

    Update maze-labirint.py

--- maze-labirint.py
+++ maze-labirint.py
@@ -1,17 +1,25 @@
 from pygame import *
 
-class GameSprite():
+class GameSprite(sprite.Sprite):
     def __init__(self, img, cord_x, cord_y, width, height, speed):
         self.image = transform.scale(image.load(img), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = cord_x
+        self.rect.y = cord_y
         self.speed = speed
-        self.x = cord_x
-        self.y = cord_y
         self.width = width
         self.height = height
 
     def show(self):
         window.blit( self.image, (self.x, self.y) )
 
+    def collide(self, sprite):
+        distance_x = abs(self.x - sprite.x)
+        distance_y = abs(self.y - sprite.y)
+        if distance_x < 50 and distance_y < 50:
+            return True
+        return False
+    
 class Player(GameSprite):
     def move(self):
         keys = key.get_pressed() 
@@ -29,9 +37,10 @@ display.set_caption('Лабиринт')
 
 back = transform.scale(image.load('back.jpg'), (700,500))
 
-player = Player('player.png', 100, 100, 50, 50, 5)
+player = Player('player.png', 100, 100, 50, 50, 10)
 enemy = GameSprite('enemy.png', 400, 100, 50, 50, 5)
 
+clock = time.Clock()
 while True:
     for some_event in event.get():
         if some_event.type == QUIT:
@@ -42,4 +51,9 @@ while True:
     enemy.show()
     player.move()
 
+    if player.collide(enemy):
+        player.x = 100
+        player.y = 100
+
     display.update()
+    clock.tick(60)
