747ed218cd6b3d4b07f7b641b1f81f67cf685364
AlgoClassWork <kyxec.play@gmail.com>
2025-12-07 10:24:08 +0300

    Update maze-labirint.py

--- maze-labirint.py
+++ maze-labirint.py
@@ -25,13 +25,30 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < 500 - self.height:
             self.rect.y += self.speed
 
+class Wall(sprite.Sprite):
+    def __init__(self, cord_x, cord_y, width, height):
+        self.width = width
+        self.height = height
+        self.image = Surface((width, height))
+        self.image.fill((255, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = cord_x
+        self.rect.y = cord_y
+
+    def show(self):
+        window.blit( self.image, (self.rect.x, self.rect.y) )
+
+# Создание окна
 window = display.set_mode( size=(700, 500) )
 display.set_caption('Лабиринт')
-
+# Загрузка фона
 back = transform.scale(image.load('back.jpg'), (700,500))
-
+# Создание спрайтов
 player = Player('player.png', 100, 100, 50, 50, 10)
 enemy = GameSprite('enemy.png', 400, 100, 50, 50, 5)
+wall1 = Wall(100, 200, 400, 20)
+wall2 = Wall(100, 200, 20, 300)
+wall3 = Wall(500, 100, 20, 300)
 
 clock = time.Clock()
 while True:
@@ -40,10 +57,15 @@ while True:
             exit()
 
     window.blit(back, (0, 0))
+    # Отображение спрайтов
     player.show()
     enemy.show()
+    wall1.show()
+    wall2.show()
+    wall3.show()
+    # Движение спрайтов
     player.move()
-
+    
     if sprite.collide_rect(player, enemy):
         player.rect.x = 100
         player.rect.y = 100
