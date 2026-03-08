12db909a77f5cb2dd6f9b3a92eff669fc6a1263d
AlgoClassWork <kyxec.play@gmail.com>
2026-03-08 17:17:30 +0600

    Update pyqt_fix.py

--- pyqt_fix.py
+++ pyqt_fix.py
@@ -1,15 +1,22 @@
 import os
 import PyQt5
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 # Что бы работал PyQt5
 pyqt_path = os.path.dirname(PyQt5.__file__)
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(pyqt_path, "Qt5", "plugins", "platforms")
 
-
 # Интерфейс
 app = QApplication([])
 window = QWidget()
+window.setWindowTitle("Генератор победителей")
+text = QLabel("Нажмите на кнопку, чтобы сгенерировать победителя!")
+winner = QLabel("Победитель: ")
+# Размещение
+layout = QVBoxLayout()
+layout.addWidget(text)
+layout.addWidget(winner)
+window.setLayout(layout)
 
 window.show()
 app.exec()
