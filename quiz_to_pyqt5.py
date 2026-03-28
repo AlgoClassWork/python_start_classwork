38eceea89af9f4befa4c729e6519e5166f0f4e4c
AlgoClassWork <kyxec.play@gmail.com>
2026-03-28 16:38:26 +0600

    Update quiz_to_pyqt5.py

--- quiz_to_pyqt5.py
+++ quiz_to_pyqt5.py
@@ -1,45 +1,98 @@
-from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QRadioButton, QGroupBox, QPushButton,
-    QVBoxLayout, QHBoxLayout
-)
-# Созднание элементов интерфейса
-app = QApplication([])
-window = QWidget()
-window.setWindowTitle('Викторина')
+import os
+import PyQt5
 
-question_label = QLabel('Самая высокая гора в мире?')
-button = QPushButton('Ответить')
-# Группа для ответов
-answer_group = QGroupBox('Ответы')
-answer1_radio = QRadioButton('Эверест')
-answer2_radio = QRadioButton('Килиманджаро')
-answer3_radio = QRadioButton('Макалу')
-answer4_radio = QRadioButton('Лхоцзе')
-answer_layout = QVBoxLayout()
-answer_h1_layout = QHBoxLayout()
-answer_h2_layout = QHBoxLayout()
-answer_layout.addLayout(answer_h1_layout)
-answer_layout.addLayout(answer_h2_layout)
-answer_h1_layout.addWidget(answer1_radio)
-answer_h1_layout.addWidget(answer2_radio)
-answer_h2_layout.addWidget(answer3_radio)
-answer_h2_layout.addWidget(answer4_radio)
-answer_group.setLayout(answer_layout)
-# Группа для результата
-result_group = QGroupBox('Результат')
-result_label = QLabel('Правильный ответ: Эверест')
-result_layout = QVBoxLayout()
-result_layout.addWidget(result_label)
-result_group.setLayout(result_layout)
-# Расположение элементов интерфейса
-main_layout = QVBoxLayout()
-main_layout.addWidget(question_label)
-main_layout.addWidget(answer_group)
-main_layout.addWidget(result_group)
-main_layout.addWidget(button)
-window.setLayout(main_layout)
-# Стилизация элементов интерфейса
-# Запуск приложения
-result_group.hide()
+# Чтобы работал PyQt5
+pyqt_path = os.path.dirname(PyQt5.__file__)
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(pyqt_path, "Qt5", "plugins", "platforms")
+
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel)
+
+
+app = QApplication([])
+
+
+# Создаем панель вопроса
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Самый сложный вопрос в мире!')
+
+
+RadioGroupBox = QGroupBox("Варианты ответов")
+
+
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_4)
+
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+
+RadioGroupBox.setLayout(layout_ans1)
+
+
+# Создаем панель результата
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+
+# Размещаем все виджеты в окне:
+layout_line1 = QHBoxLayout() # вопрос
+layout_line2 = QHBoxLayout() # варианты ответов или результат теста
+layout_line3 = QHBoxLayout() # кнопка "Ответить"
+
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+# Размещаем в одной строке обе панели, одна из них будет скрываться, другая показываться:
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+RadioGroupBox.hide() # эту панель мы уже видели, скроем, посмотрим, как получилась панель с ответом
+
+
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+
+
+# Теперь созданные строки разместим друг под другой:
+layout_card = QVBoxLayout()
+
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+
+
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memory Card')
 window.show()
+
+
 app.exec()
