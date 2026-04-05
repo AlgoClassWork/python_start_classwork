edad29d6d6bde7c7355499cac3d6c5080629bcdb
AlgoClassWork <kyxec.play@gmail.com>
2026-04-05 17:43:59 +0600

    Create movie_list_film.py

---
+++ movie_list_film.py
@@ -0,0 +1,46 @@
+from PyQt5.QtWidgets import (
+     QApplication, QWidget, QTextEdit, QListWidget, QPushButton,
+     QHBoxLayout, QVBoxLayout
+)
+
+# Создание интерфейса
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Мой список фильмов')
+window.resize(800, 600)
+
+description_field = QTextEdit() 
+description_field.setPlaceholderText('Опишите фильм здесь...')
+
+movie_list = QListWidget()
+movie_list.addItem('Фильм 1')
+movie_list.addItem('Фильм 2')
+movie_list.addItem('Фильм 3')
+add_film_button = QPushButton('Добавить фильм')
+delete_film_button = QPushButton('Удалить фильм')
+
+genre_list = QListWidget()
+genre_list.addItem('Жанр 1')
+genre_list.addItem('Жанр 2')
+genre_list.addItem('Жанр 3')
+
+# Разметка интерфейса
+main_layout = QHBoxLayout()
+list_layout = QVBoxLayout()
+list_buttons_layout = QHBoxLayout()
+
+main_layout.addLayout(list_layout)
+list_layout.addWidget(movie_list)
+list_layout.addLayout(list_buttons_layout)
+list_buttons_layout.addWidget(add_film_button)
+list_buttons_layout.addWidget(delete_film_button)
+
+list_layout.addWidget(genre_list)
+
+main_layout.addWidget(description_field)
+
+window.setLayout(main_layout)
+
+# Запуск
+window.show()
+app.exec()
