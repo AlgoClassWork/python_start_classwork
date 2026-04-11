984e78f5d18dbd62ed8fe938b07e36fefb606800
AlgoClassWork <kyxec.play@gmail.com>
2026-04-11 17:33:44 +0600

    Update movie_list_film.py

--- movie_list_film.py
+++ movie_list_film.py
@@ -1,3 +1,11 @@
+import os
+import PyQt5
+
+# Чтобы работал PyQt5
+pyqt_path = os.path.dirname(PyQt5.__file__)
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(pyqt_path, "Qt5", "plugins", "platforms")
+
+
 from PyQt5.QtWidgets import (
      QApplication, QWidget, QTextEdit, QListWidget, QPushButton, QLineEdit,
      QHBoxLayout, QVBoxLayout
@@ -12,17 +20,11 @@ window.resize(800, 600)
 description_field = QTextEdit() 
 description_field.setPlaceholderText('Введите описание фильма...')
 
-movie_list = QListWidget()
-movie_list.addItem('Фильм 1')
-movie_list.addItem('Фильм 2')
-movie_list.addItem('Фильм 3')
+film_list = QListWidget()
 add_film_button = QPushButton('Добавить фильм')
 delete_film_button = QPushButton('Удалить фильм')
 
 genre_list = QListWidget()
-genre_list.addItem('Жанр 1')
-genre_list.addItem('Жанр 2')
-genre_list.addItem('Жанр 3')
 search_field = QLineEdit()
 search_field.setPlaceholderText('Введите жанр для поиска...')
 add_genre_button = QPushButton('Добавить жанр')
@@ -37,7 +39,7 @@ list_buttons_layout2 = QHBoxLayout()
 
 main_layout.addLayout(list_layout)
 
-list_layout.addWidget(movie_list)
+list_layout.addWidget(film_list)
 list_layout.addLayout(list_buttons_layout)
 list_buttons_layout.addWidget(add_film_button)
 list_buttons_layout.addWidget(delete_film_button)
@@ -53,6 +55,11 @@ main_layout.addWidget(description_field)
 
 window.setLayout(main_layout)
 
+# Функционал приложения
+
+# Подписки на события
+
 # Запуск
+
 window.show()
 app.exec()
