b7a19da9448bee2ea744e708b43584df2b1cc826
AlgoClassWork <kyxec.play@gmail.com>
2026-06-25 16:24:21 +0600

    Configure PyQt5 environment variable
    Set environment variable for PyQt5 platform plugins.

---
+++ photoshop_clone.py
@@ -0,0 +1,5 @@
+import os
+import PyQt5
+# Чтобы работал PyQt5
+pyqt_path = os.path.dirname(PyQt5.__file__)
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(pyqt_path, "Qt5", "plugins", "platforms")
