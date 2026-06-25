a4f61191a583fed0caffbe91f9a82a7999b7a5ae
AlgoClassWork <kyxec.play@gmail.com>
2026-06-25 16:51:19 +0600

    Update photoshop_clone.py

--- photoshop_clone.py
+++ photoshop_clone.py
@@ -30,6 +30,28 @@ button_mirror = QPushButton('Зеркало')
 button_sharp = QPushButton('Резкость')
 button_gray = QPushButton('Ч | Б')
 
+# Разметка
+layout_main = QHBoxLayout()
+layout_left = QVBoxLayout()
+layout_right = QVBoxLayout()
+layout_buttons = QHBoxLayout()
+
+layout_main.addLayout(layout_left)
+layout_main.addLayout(layout_right)
+layout_right.addLayout(layout_buttons)
+
+layout_left.addWidget(list_images)
+layout_left.addWidget(button_folder)
+
+layout_right.addWidget(image_preview)
+
+layout_buttons.addWidget(button_left)
+layout_buttons.addWidget(button_right)
+layout_buttons.addWidget(button_mirror)
+layout_buttons.addWidget(button_sharp)
+layout_buttons.addWidget(button_gray)
+
 # Запуск приложения
+window.setLayout(layout_main)
 window.show()
 app.exec()
