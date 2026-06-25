ed88e897d29b4d7c7bd4d6cc91ab144661a360dc
AlgoClassWork <kyxec.play@gmail.com>
2026-06-25 16:31:43 +0600

    Update photoshop_clone.py

--- photoshop_clone.py
+++ photoshop_clone.py
@@ -3,3 +3,22 @@ import PyQt5
 # Чтобы работал PyQt5
 pyqt_path = os.path.dirname(PyQt5.__file__)
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(pyqt_path, "Qt5", "plugins", "platforms")
+
+# Импорты
+from PIL import Image, ImageFilter # pip install pillow
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QVBoxLayout, QHBoxLayout,
+    QPushButton, QListWidget,
+    QLabel, QFileDialog
+)
+
+# Создание приложения
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Редактор')
+
+# Запуск приложения
+window.show()
+app.exec()
